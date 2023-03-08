9e8eee70a07c53fb17e28fd7a70add96fffea774
liviu-silion <liviu.silion@kappas.it>
2023-03-08 09:57:18 +0100

    Remove unncessesary blank spaces

--- PlannerCRM/Server/Program.cs
+++ PlannerCRM/Server/Program.cs
@@ -2,22 +2,16 @@ using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+if (app.Environment.IsDevelopment()) {
     app.UseWebAssemblyDebugging();
-}
-else
-{
+} else {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -27,7 +21,6 @@ app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
 
 app.UseRouting();
-
 
 app.MapRazorPages();
 app.MapControllers();
