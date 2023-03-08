69a2ebe42c31edd8eeb8a122ed4377e92b63b816
liviu-silion <liviu.silion@kappas.it>
2023-03-08 13:10:14 +0100

    Add employee CRUD methods

--- PlannerCRM/Server/Controllers/AccountController.cs
+++ PlannerCRM/Server/Controllers/AccountController.cs
@@ -54,4 +54,63 @@ public class AccountController : ControllerBase
     public async Task Logout() {
         await _signInManager.SignOutAsync();
     }
+    
+    [HttpPost("add/employee")]
+    public async Task<ActionResult> AddEmployee(Employee employee) {
+        if (string.IsNullOrEmpty(employee.Email)) {
+            return BadRequest("""Il campo "Email" non deve essere vuoto!""");
+        } else if (string.IsNullOrEmpty(employee.Password)) {
+            return BadRequest("""Il campo "Password" non deve essere vuoto!""");
+        } else if (string.IsNullOrEmpty(employee.FirstName)) {
+            return BadRequest("""Il campo "Nome" non deve essere vuoto!""");
+        } else if (string.IsNullOrEmpty(employee.LastName)) {
+            return BadRequest("""Il campo "Cognome" non deve essere vuoto!""");
+        } else if (employee.Birthday == null) {
+            return BadRequest("""Il campo "Data di nascita" non deve essere vuoto!""");
+        } else if (string.IsNullOrEmpty(employee.NumericCode)) {
+            return BadRequest("""Il campo "Codice numerico" non deve essere vuoto!""");
+        } else if (employee.StartDate == null) {
+            return BadRequest("""Il campo "Data di inizio" non deve essere vuoto!""");
+        } else if (employee.Salaries == null) {
+            return BadRequest("""Il campo "Tariffa oraria" non deve essere vuoto!""");
+        } else {
+            await _repo.AddAsync(employee);
+            return Ok();
+        }
+    }
+
+    [HttpPut("edit/employee")]
+    public async Task<ActionResult> EditEmployee(Employee employee) {
+        if (string.IsNullOrEmpty(employee.Email)) {
+            return BadRequest("""Il campo "Email" non deve essere vuoto!""");
+        } else if (string.IsNullOrEmpty(employee.Password)) {
+            return BadRequest("""Il campo "Password" non deve essere vuoto!""");
+        } else if (string.IsNullOrEmpty(employee.FirstName)) {
+            return BadRequest("""Il campo "Nome" non deve essere vuoto!""");
+        } else if (string.IsNullOrEmpty(employee.LastName)) {
+            return BadRequest("""Il campo "Cognome" non deve essere vuoto!""");
+        } else if (employee.Birthday == null) {
+            return BadRequest("""Il campo "Data di nascita" non deve essere vuoto!""");
+        } else if (string.IsNullOrEmpty(employee.NumericCode)) {
+            return BadRequest("""Il campo "Codice numerico" non deve essere vuoto!""");
+        } else if (employee.StartDate == null) {
+            return BadRequest("""Il campo "Data di inizio" non deve essere vuoto!""");
+        } else if (employee.Salaries == null) {
+            return BadRequest("""Il campo "Tariffa oraria" non deve essere vuoto!""");
+        } else {
+            await _repo.EditAsync(employee.Id, employee);
+            return Ok();
+        }
+    }
+
+    [HttpDelete("delete/employee")]
+    public async Task<ActionResult> DeleteEmployee(Employee employee) {
+        await _repo.DeleteAsync(employee.Id);
+        return Ok();
+    }
+
+    [HttpGet("get/all")]
+    public async Task<IEnumerable<Employee>> GetAll() {
+        return await _repo.GetAllAsync();
+    }
 }
