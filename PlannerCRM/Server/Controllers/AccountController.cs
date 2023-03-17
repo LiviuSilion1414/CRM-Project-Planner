f992e87993b22ea8579cf5d2b8bbade69ce15b5b
liviu-silion <liviu.silion@kappas.it>
2023-03-17 15:38:38 +0100

    Fix account manager add user method

--- PlannerCRM/Server/Controllers/AccountController.cs
+++ PlannerCRM/Server/Controllers/AccountController.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using PlannerCRM.Server.Models;
 using PlannerCRM.Server.Services.ConcreteClasses;
-using PlannerCRM.Shared;
 using PlannerCRM.Shared.DTOs;
 
 namespace PlannerCRM.Server.Controllers;
@@ -23,7 +22,7 @@ public class AccountController : ControllerBase
     }
 
     [HttpPost("login")]
-    public async Task<ActionResult> Login(Employee employee) {
+    public async Task<ActionResult> Login(EmployeeLoginDTO employee) {
         var user = await _userManager.FindByEmailAsync(employee.Email);
 
         if (user == null) {
@@ -40,11 +39,34 @@ public class AccountController : ControllerBase
         }
     }
 
-    [HttpPost]
-    [Route("employee/role")]
-    public async Task<IList<string>> GetUserRole(EmployeeLoginDTO model)
+    [HttpPost("add/user")]
+    public async Task<IActionResult> AddUser(EmployeeAddDTO employeeAdd)
     {
-        var user = await _userManager.FindByEmailAsync(model.Email);
+        if (!ModelState.IsValid) {
+            return BadRequest();
+        }
+
+        var person = await _userManager.FindByEmailAsync(employeeAdd.Email);
+
+        if (person == null) {
+            var user = new IdentityUser {
+                Email = employeeAdd.Email,
+                EmailConfirmed = true,
+                UserName = employeeAdd.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, employeeAdd.Password);
+ 
+            await _userManager.AddToRoleAsync(user, employeeAdd.Role.ToString());
+        }
+        return Ok();
+    }
+
+    [HttpGet]
+    [Route("user/role")]
+    public async Task<IList<string>> GetUserRole()
+    {
+        var user = await _userManager.FindByNameAsync(User.Identity.Name);
         var roles = await _userManager.GetRolesAsync(user);
         return roles;
     }
@@ -52,64 +74,5 @@ public class AccountController : ControllerBase
     [HttpPost("logout")]
     public async Task Logout() {
         await _signInManager.SignOutAsync();
-    }
-    
-    [HttpPost("add/employee")]
-    public async Task<ActionResult> AddEmployee(Employee employee) {
-        if (string.IsNullOrEmpty(employee.Email)) {
-            return BadRequest("""Il campo "Email" non deve essere vuoto!""");
-        } else if (string.IsNullOrEmpty(employee.Password)) {
-            return BadRequest("""Il campo "Password" non deve essere vuoto!""");
-        } else if (string.IsNullOrEmpty(employee.FirstName)) {
-            return BadRequest("""Il campo "Nome" non deve essere vuoto!""");
-        } else if (string.IsNullOrEmpty(employee.LastName)) {
-            return BadRequest("""Il campo "Cognome" non deve essere vuoto!""");
-        } else if (employee.Birthday == null) {
-            return BadRequest("""Il campo "Data di nascita" non deve essere vuoto!""");
-        } else if (string.IsNullOrEmpty(employee.NumericCode)) {
-            return BadRequest("""Il campo "Codice numerico" non deve essere vuoto!""");
-        } else if (employee.StartDate == null) {
-            return BadRequest("""Il campo "Data di inizio" non deve essere vuoto!""");
-        } else if (employee.Salaries == null) {
-            return BadRequest("""Il campo "Tariffa oraria" non deve essere vuoto!""");
-        } else {
-            await _repo.AddAsync(employee);
-            return Ok();
-        }
-    }
-
-    [HttpPut("edit/employee")]
-    public async Task<ActionResult> EditEmployee(Employee employee) {
-        if (string.IsNullOrEmpty(employee.Email)) {
-            return BadRequest("""Il campo "Email" non deve essere vuoto!""");
-        } else if (string.IsNullOrEmpty(employee.Password)) {
-            return BadRequest("""Il campo "Password" non deve essere vuoto!""");
-        } else if (string.IsNullOrEmpty(employee.FirstName)) {
-            return BadRequest("""Il campo "Nome" non deve essere vuoto!""");
-        } else if (string.IsNullOrEmpty(employee.LastName)) {
-            return BadRequest("""Il campo "Cognome" non deve essere vuoto!""");
-        } else if (employee.Birthday == null) {
-            return BadRequest("""Il campo "Data di nascita" non deve essere vuoto!""");
-        } else if (string.IsNullOrEmpty(employee.NumericCode)) {
-            return BadRequest("""Il campo "Codice numerico" non deve essere vuoto!""");
-        } else if (employee.StartDate == null) {
-            return BadRequest("""Il campo "Data di inizio" non deve essere vuoto!""");
-        } else if (employee.Salaries == null) {
-            return BadRequest("""Il campo "Tariffa oraria" non deve essere vuoto!""");
-        } else {
-            await _repo.EditAsync(employee.Id, employee);
-            return Ok();
-        }
-    }
-
-    [HttpDelete("delete/employee")]
-    public async Task<ActionResult> DeleteEmployee(Employee employee) {
-        await _repo.DeleteAsync(employee.Id);
-        return Ok();
-    }
-
-    [HttpGet("get/all")]
-    public async Task<IEnumerable<Employee>> GetAll() {
-        return await _repo.GetAllAsync();
     }
 }
