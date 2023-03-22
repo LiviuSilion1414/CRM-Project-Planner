ba9c26bd575122829b7c24cfb2a0a1438d9bb955
liviu-silion <liviu.silion@kappas.it>
2023-03-22 10:16:08 +0100

    Add dtos to service repo

--- PlannerCRM/Server/Services/WorkOrderRepository.cs
+++ PlannerCRM/Server/Services/WorkOrderRepository.cs
@@ -1,6 +1,8 @@
 using PlannerCRM.Server.DataAccess;
 using PlannerCRM.Server.Models;
 using Microsoft.EntityFrameworkCore;
+using PlannerCRM.Shared.DTOs.Workorder.Forms;
+using PlannerCRM.Shared.DTOs.Workorder.Views;
 
 namespace PlannerCRM.Server.Services.ConcreteClasses;
 
@@ -11,8 +13,12 @@ public class WorkOrderRepository
     public WorkOrderRepository(AppDbContext db) {
         _db = db;
     }
-    public async Task AddAsync(WorkOrder entity) {
-        _db.WorkOrders.Add(entity);
+    public async Task AddAsync(WorkorderAddFormDTO entity) {
+        _db.WorkOrders.Add(new WorkOrder {
+            Name = entity.Name,
+            StartDate = DateTime.Parse(entity.StartDate),
+            FinishDate = DateTime.Parse(entity.FinishDate)
+        });
 
         await _db.SaveChangesAsync();
     }
@@ -28,7 +34,7 @@ public class WorkOrderRepository
         await _db.SaveChangesAsync();
     }
 
-    public async Task<bool> EditAsync(WorkOrder entity) {
+    public async Task<bool> EditAsync(WorkorderEditFormDTO entity) {
         var model = await _db.WorkOrders.SingleOrDefaultAsync(w => w.Id == entity.Id);
         
         if (model == null) {
@@ -37,23 +43,32 @@ public class WorkOrderRepository
 
         model.Id = entity.Id;
         model.Name = entity.Name;
-        model.StartDate = entity.StartDate;
-        model.Activities = entity.Activities; 
+        model.StartDate = DateTime.Parse(entity.StartDate);
+        model.FinishDate = DateTime.Parse(entity.FinishDate);
 
         await _db.SaveChangesAsync();
         return true;
     }
 
-    public async Task<WorkOrder> GetAsync(int id) {
+    public async Task<WorkorderViewDTO> GetAsync(int id) {
         return await _db.WorkOrders
-            .Include(wo => wo.Activities)
-            .ThenInclude(a => a.EmployeeActivity)
-            .SingleOrDefaultAsync(w => w.Id == id);
+            .Select(e => new WorkorderViewDTO {
+                Id = e.Id,
+                Name = e.Name,
+                StartDate = e.StartDate.ToShortDateString(),
+                FinishDate = e.FinishDate.ToShortDateString()
+            })
+            .SingleOrDefaultAsync(e => e.Id == id);
     }
 
-    public async Task<List<WorkOrder>> GetAllAsync() {
+    public async Task<List<WorkorderViewDTO>> GetAllAsync() {
         return await _db.WorkOrders
-            .Include(a => a.Activities)
+            .Select(e => new WorkorderViewDTO {
+                Id = e.Id,
+                Name = e.Name,
+                StartDate = e.StartDate.ToShortDateString(),
+                FinishDate = e.FinishDate.ToShortDateString()
+            })
             .ToListAsync();
     }
 }
