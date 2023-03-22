a3f827373d5af046eb192d7bfbcddacb2271ce9b
liviu-silion <liviu.silion@kappas.it>
2023-03-22 10:25:36 +0100

    Add workorder controller

---
+++ PlannerCRM/Server/Controllers/WorkOrderController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using PlannerCRM.Server.Services.ConcreteClasses;
+using PlannerCRM.Shared.DTOs.Workorder.Forms;
+using PlannerCRM.Shared.DTOs.Workorder.Views;
+
+namespace PlannerCRM.Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WorkOrderController : ControllerBase
+{
+    private readonly WorkOrderRepository _repo;
+
+    public WorkOrderController(WorkOrderRepository repo) {
+        _repo = repo;
+    }
+
+    [HttpPost("add")]
+    public async Task AddWorkorder(WorkorderAddFormDTO model) {
+        await _repo.AddAsync(model);
+    }
+
+    [HttpPut("edit")]
+    public async Task EditWorkorder(WorkorderEditFormDTO model) {
+        await _repo.EditAsync(model);
+    }
+
+    [HttpDelete("delete/{id}")]
+    public async Task DeleteWorkorder(int id) {
+        await _repo.DeleteAsync(id);
+    }
+
+    [HttpGet("get/{id}")]
+    public async Task<WorkorderViewDTO> GetById(int id) {
+        return await _repo.GetAsync(id);
+    }
+
+    [HttpGet("get/all")]
+    public async Task<List<WorkorderViewDTO>> GetAll() {
+        return await _repo.GetAllAsync();
+    }
+}
