d6a096e72460727ec5cd8001f3ae2918f8bf0199
liviu-silion <liviu.silion@kappas.it>
2023-03-26 14:24:10 +0200

    Activate services in Program.cs

--- PlannerCRM/Client/Program.cs
+++ PlannerCRM/Client/Program.cs
@@ -1,10 +1,19 @@
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PlannerCRM.Client;
+using PlannerCRM.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+
+    builder.Services.AddOptions();
+    builder.Services.AddAuthorizationCore();
+    builder.Services.AddScoped<CustomAuthState>();
+    builder.Services.AddScoped<AuthenticationStateProvider>(s => s.GetRequiredService<CustomAuthState>());
+    builder.Services.AddScoped<AuthService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
