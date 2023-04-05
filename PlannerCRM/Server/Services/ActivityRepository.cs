32247812330e78409f9d7a292de489c7ad34f973
liviu-silion <liviu.silion@kappas.it>
2023-04-05 16:22:14 +0200

    Update ActivityRepo

--- PlannerCRM/Server/Services/ActivityRepository.cs
+++ PlannerCRM/Server/Services/ActivityRepository.cs
@@ -1,6 +1,8 @@
 using PlannerCRM.Server.DataAccess;
 using PlannerCRM.Server.Models;
 using Microsoft.EntityFrameworkCore;
+using PlannerCRM.Shared.DTOs.ActivityDto.Forms;
+using PlannerCRM.Shared.DTOs.ActivityDto.Views;
 
 namespace PlannerCRM.Server.Services;
 
@@ -11,8 +13,14 @@ public class ActivityRepository
     public ActivityRepository(AppDbContext db) {
         _db = db;
     }
-    public async Task AddAsync(Activity entity) {
-        _db.Activities.Add(entity);
+    public async Task AddAsync(ActivityForm entity) {
+        _db.Activities.Add(new Activity {
+            Id = entity.Id,
+            Name = entity.Name,
+            StartDate = entity.StartDate,
+            FinishDate = entity.FinishDate,
+            WorkOrderId = entity.WorkorderId
+        });
         
         await _db.SaveChangesAsync();
     }
@@ -28,7 +36,7 @@ public class ActivityRepository
         await _db.SaveChangesAsync();
     }
 
-    public async Task<bool> EditAsync(Activity entity) {
+    public async Task<bool> EditAsync(ActivityForm entity) {
         var model = await _db.Activities.SingleOrDefaultAsync(a => a.Id == entity.Id);
         if (model == null) {
             return false;
@@ -38,18 +46,33 @@ public class ActivityRepository
         model.Name = entity.Name;
         model.StartDate = entity.StartDate;
         model.FinishDate = entity.FinishDate;
-        model.EmployeeActivity = entity.EmployeeActivity;
-        model.WorkOrderId = entity.WorkOrderId;
+        model.WorkOrderId = entity.WorkorderId;
 
         await _db.SaveChangesAsync();
         return true;
     }
 
-    public async Task<Activity> GetAsync(int id) {
-        return await _db.Activities.SingleOrDefaultAsync(a => a.Id == id);
+    public async Task<ActivityViewDTO> GetAsync(int id) {
+        return await _db.Activities
+            .Select(e => new ActivityViewDTO {
+                Id = e.Id,
+                Name = e.Name,
+                StartDate = e.StartDate,
+                FinishDate = e.FinishDate,
+                WorkOrderId = e.WorkOrderId
+            })
+            .SingleOrDefaultAsync(a => a.Id == id);
     }
 
-    public async Task<List<Activity>> GetAllAsync() {
-        return await _db.Activities.ToListAsync();
+    public async Task<List<ActivityViewDTO>> GetAllAsync() {
+        return await _db.Activities
+            .Select(e => new ActivityViewDTO {
+                Id = e.Id,
+                Name = e.Name,
+                StartDate = e.StartDate,
+                FinishDate = e.FinishDate,
+                WorkOrderId = e.WorkOrderId
+            })
+            .ToListAsync();
     }
 }
