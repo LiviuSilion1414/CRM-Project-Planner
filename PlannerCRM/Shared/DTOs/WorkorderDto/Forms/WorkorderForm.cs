1ac5395c369d4d2f5b229f175520c6da07a2a9a0
liviu-silion <liviu.silion@kappas.it>
2023-05-03 18:57:33 +0200

    Update WorkorderForm.cs

--- PlannerCRM/Shared/DTOs/WorkorderDto/Forms/WorkorderForm.cs
+++ PlannerCRM/Shared/DTOs/WorkorderDto/Forms/WorkorderForm.cs
@@ -1,18 +1,23 @@
 using System.ComponentModel.DataAnnotations;
+using PlannerCRM.Shared.Attributes;
+using static PlannerCRM.Shared.Constants.ConstantValues;
 
 namespace PlannerCRM.Shared.DTOs.Workorder.Forms;
 
 public partial class WorkorderForm
 {
-    [Required]
     public int Id { get; set; }
     
-    [Required]
+    [Required(ErrorMessage = """ Campo "Nome" richiesto. """)]
     public string Name { get; set; }
     
-    [Required]
+    [Required(ErrorMessage = """ Campo "Data d'inizio" richiesto. """)]
+    [StartDateRange(MIN_WORKORDER_MONTH_CONTRACT, MAX_WORKORDER_MONTH_CONTRACT, 
+        ErrorMessage = "Il periodo contrattuale dev'essere tra 3 e 12 mesi.")]
     public DateTime StartDate { get; set; }
     
-    [Required]
+    [Required(ErrorMessage = """ Campo "Data di fine" richiesto. """)]
+    [FinishDateRange(MIN_WORKORDER_MONTH_CONTRACT, MAX_WORKORDER_MONTH_CONTRACT, 
+        ErrorMessage = "Il periodo contrattuale dev'essere tra 3 e 12 mesi.")]
     public DateTime FinishDate { get; set; }
 }
