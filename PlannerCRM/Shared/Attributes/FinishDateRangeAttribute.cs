b840768455ca60a4a9adf6679b027b8f639da2db
liviu-silion <liviu.silion@kappas.it>
2023-05-03 18:56:09 +0200

    Create FinishDateRangeAttribute.cs

---
+++ PlannerCRM/Shared/Attributes/FinishDateRangeAttribute.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+using static PlannerCRM.Shared.Constants.ConstantValues;
+
+namespace PlannerCRM.Shared.Attributes;
+
+public class FinishDateRangeAttribute : ValidationAttribute
+{
+    private int _Minimum { get; set; }
+    private int _Maximum { get; set; }
+
+    public FinishDateRangeAttribute(int minimum, int maximum) {
+        _Minimum = minimum;
+        _Maximum = maximum;
+    }
+
+    public override bool IsValid(object value) {
+        if (value.GetType() == typeof(DateTime)) {
+            var date = Convert.ToDateTime(value);
+            if (date > CURRENT_DATE) {
+                if ((date.Month >= _Minimum) && (date.Month <= _Maximum)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+}
