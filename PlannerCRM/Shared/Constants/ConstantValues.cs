a7c9d6f57d2f0bc2338b303b28421d1fd5ef63cb
liviu-silion <liviu.silion@kappas.it>
2023-05-03 15:07:00 +0200

    Create class with most commonly used values

---
+++ PlannerCRM/Shared/Constants/ConstantValues.cs
@@ -0,0 +1,12 @@
+namespace PlannerCRM.Shared.Constants;
+
+public static class ConstantValues
+{
+    public static DateTime CURRENT_DATE = DateTime.Now;
+    public const int MAJOR_AGE = 18;
+    public const int MAX_AGE = 50;
+    public const int MINIMUM_YEAR = 1973;
+    public const int CURRENT_YEAR = 2023;
+    public const int PASS_MIN_LENGTH = 6;
+    public const int PASS_MAX_LENGTH = 16;
+}
