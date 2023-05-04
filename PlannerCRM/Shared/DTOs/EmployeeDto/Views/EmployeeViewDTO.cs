0cf546641cac40550bbc87dba86a76f740161d0a
liviu-silion <liviu.silion@kappas.it>
2023-05-04 18:49:02 +0200

    Add missing properties

--- PlannerCRM/Shared/DTOs/EmployeeDto/Views/EmployeeViewDTO.cs
+++ PlannerCRM/Shared/DTOs/EmployeeDto/Views/EmployeeViewDTO.cs
@@ -1,12 +1,18 @@
+using PlannerCRM.Shared.DTOs.ActivityDto.Forms;
+using PlannerCRM.Shared.DTOs.EmployeeDto.Forms;
+
 namespace PlannerCRM.Shared.DTOs.EmployeeDto.Views;
 
 public class EmployeeViewDTO
 { 
     public int Id { get; set; }
     public string Email { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
     public string FullName { get; set; }
     public string Role { get; set; }
     public DateTime BirthDay { get; set; }
     public DateTime StartDate { get; set; }
-    public decimal HourPay { get; set; }
+    public List<EmployeeSalaryDTO> EmployeeSalaries { get; set; }
+    public List<EmployeeActivityDTO> EmployeeActivities { get; set; }
 }
