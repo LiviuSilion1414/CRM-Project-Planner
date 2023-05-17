a1a51cd0dd5d3ae5756570a8f199b3cb9e55a052
liviu-silion <liviu.silion@kappas.it>
2023-05-17 15:30:20 +0200

    Create constant exception messages

---
+++ PlannerCRM/Shared/Constants/ExceptionsMessages.cs
@@ -0,0 +1,17 @@
+using System.Runtime.CompilerServices;
+
+namespace PlannerCRM.Shared.Constants;
+
+public static class ExceptionsMessages
+{
+    public const string NULL_OBJECT = "Oggetto dto null.";
+    public const string NULL_PARAM = "Parametri dto null.";
+    public const string NULL_PROP = "Proprietà di tipo null";
+    public const string OBJECT_ALREADY_PRESENT = "Oggetto già presente.";
+    public const string IMPOSSIBLE_SAVE_CHANGES = "Impossibile salvare le modifiche sull'oggetto.";
+    public const string IMPOSSIBLE_ADD = "Impossibile aggiungere l'oggetto.";
+    public const string IMPOSSIBLE_DELETE = "Impossibile eliminare l'oggetto.";
+    public const string IMPOSSIBLE_EDIT = "Impossibile modificare l'oggetto.";
+    public const string IMPOSSIBILE_GOING_FORWARD = "Impossibile proseguire.";
+    public const string OBJECT_NOT_FOUND = "Oggetto non trovato.";
+}
