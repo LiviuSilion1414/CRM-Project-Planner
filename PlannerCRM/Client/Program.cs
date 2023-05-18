7d0338471aab5455b61da8a0df78e2332f3cea82
liviu-silion <liviu.silion@kappas.it>
2023-05-18 17:45:45 +0200

    Register navigation lock service into Program.cs

--- PlannerCRM/Client/Program.cs
+++ PlannerCRM/Client/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<CustomAuthState>();
 builder.Services.AddScoped<AuthenticationStateProvider>(s => s.GetRequiredService<CustomAuthState>());
 builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<NavigationLockService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
