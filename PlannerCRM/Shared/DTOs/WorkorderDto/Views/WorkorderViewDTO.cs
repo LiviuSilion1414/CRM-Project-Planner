0f5ee1d7e44e8ff9c881a6c8de7587f695830ac1
liviu-silion <liviu.silion@kappas.it>
2023-06-06 09:33:39 +0200

    Apply migrations

--- PlannerCRM/Shared/DTOs/WorkorderDto/Views/WorkorderViewDTO.cs
+++ PlannerCRM/Shared/DTOs/WorkorderDto/Views/WorkorderViewDTO.cs
@@ -8,4 +8,7 @@ public class WorkOrderViewDto
     public string Name { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime FinishDate { get; set; }
+    public bool IsDeleted { get; set; }
+    public bool IsCompleted { get; set; }
+
 }
