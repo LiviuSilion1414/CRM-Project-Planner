01bbe6b43dee32635ab2d99d9c20f4a6688fe264
liviu-silion <liviu.silion@kappas.it>
2023-07-07 14:28:37 +0200

    Add global imports

---
+++ PlannerCRM/Shared/Imports/GlobalImports.cs
@@ -0,0 +1,8 @@
+global using System.ComponentModel.DataAnnotations;
+global using static PlannerCRM.Shared.Constants.ConstantValues;
+global using PlannerCRM.Shared.Models;
+global using System.Text.RegularExpressions;
+global using PlannerCRM.Shared.DTOs.ActivityDto.Forms;
+global using PlannerCRM.Shared.DTOs.EmployeeDto.Forms;
+global using PlannerCRM.Shared.DTOs.Workorder.Forms;
+global using System.Reflection;
