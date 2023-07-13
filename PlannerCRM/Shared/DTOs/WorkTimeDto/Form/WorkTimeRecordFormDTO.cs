71858a39564a5dfcb7c65e09885b183ea2644bca
liviu-silion <liviu.silion@kappas.it>
2023-07-13 18:07:03 +0200

    Add missing required attribute

--- PlannerCRM/Shared/DTOs/WorkTimeDto/Form/WorkTimeRecordFormDTO.cs
+++ PlannerCRM/Shared/DTOs/WorkTimeDto/Form/WorkTimeRecordFormDTO.cs
@@ -6,13 +6,25 @@ public class WorkTimeRecordFormDto
 
 {
     public int Id { get; set; }
-    public DateTime Date { get; set; }
-    public int Hours { get; set; }
-    public Decimal TotalPrice { get; set; }
 
+    [Required(ErrorMessage = """ Campo "Data" richiesto. """)]
+    public DateTime Date { get; set; }
+
+    [Required(ErrorMessage = """ Campo "Tariffa oraria" richiesto. """)]
+    public int Hours { get; set; }
+
+    [Required(ErrorMessage = """ Campo "Prezzo totale" richiesto.""")]
+    public decimal TotalPrice { get; set; }
+
+    [Required(ErrorMessage = """ Campo "Attività" richiesto. """)]
     public int ActivityId { get; set; }
+
+    [Required(ErrorMessage = """ Campo "Commessa" richiesto. """)]
     public int WorkOrderId { get; set; }
 
+    [Required(ErrorMessage = """ Campo "Impiegato" richiesto. """)]
     public int EmployeeId { get; set; }
+
+    [Required(ErrorMessage = """ Campo "Impiegato" richiesto. """)]
     public EmployeeViewDto Employee { get; set; }
 }
