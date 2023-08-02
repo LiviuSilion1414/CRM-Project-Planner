1c2f6aaee23d714d62ff4d22cdfe8d6f3a6be1cd
liviu-silion <liviu.silion@kappas.it>
2023-08-02 11:33:49 +0200

    Add titles for modals

---
+++ PlannerCRM/Shared/Feebacks/Titles.cs
@@ -0,0 +1,21 @@
+namespace PlannerCRM.Shared.Feedbacks;
+
+public class Titles
+{
+    public const string ADD_USER = "Aggiungi utente";
+    public const string EDIT_USER = "Modifica utente";
+    public const string DELETE_USER = "Elimina utente";
+    public const string DETAILS_USER = "Informazioni utente";
+        
+    public const string ADD_WORKORDER = "Aggiungi commessa";
+    public const string EDIT_WORKORDER = "Modifica commessa";
+    public const string DELETE_WORKORDER = "Elimina commessa";
+    public const string DETAILS_WORKORDER = "Informazioni commessa";
+
+    public const string ADD_ACTIVITY = "Aggiungi attività";
+    public const string EDIT_ACTIVITY = "Modifica attività";
+    public const string DELETE_ACTIVITY = "Elimina attività";
+    public const string DETAILS_ACTIVITY = "Informazioni attività";
+
+    public const string ADD_HOURS = "Aggiungi orario di lavoro";
+}
