ee883f2b3201a2d29f17775fbbb270ed2bdb1359
liviu-silion <liviu.silion@kappas.it>
2023-08-02 11:25:48 +0200

    Improve switch text/password component

---
+++ PlannerCRM/Client/Pages/ToggleView/SwitchInputType.razor.cs
@@ -0,0 +1,29 @@
+namespace PlannerCRM.Client.Pages.ToggleView;
+
+public partial class SwitchInputType : ComponentBase
+{
+    [Parameter] public EventCallback<string> Switch { get; set; }
+        
+    private bool _isSelected;
+    private string _input; 
+
+    protected override async Task OnInitializedAsync() {
+       _isSelected = false;
+       _input = Types.PASSWORD;
+
+       await Switch.InvokeAsync(_input);
+    }
+
+    private async Task Toggle() {
+        _isSelected = !_isSelected;
+
+        _input = _isSelected ? Types.TEXT : Types.PASSWORD;
+
+        await Switch.InvokeAsync(_input);
+    }
+
+    internal class Types {
+        public const string TEXT = "text";
+        public const string PASSWORD = "password";
+    }
+}
