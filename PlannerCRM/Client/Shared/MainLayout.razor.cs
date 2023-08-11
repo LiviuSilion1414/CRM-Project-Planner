85c331dbe17522e8dfbe90e790cf1004276f7696
liviu-silion <liviu.silion@kappas.it>
2023-08-11 12:03:39 +0200

    Remove bug that caused rerendering

--- PlannerCRM/Client/Shared/MainLayout.razor.cs
+++ PlannerCRM/Client/Shared/MainLayout.razor.cs
@@ -9,9 +9,9 @@ public partial class MainLayout
     [Inject] public NavigationManager NavigationManager { get; set; }
     [Inject] public LoginService LoginService { get; set; }
     
-    private readonly string _userRole;
-    private readonly CurrentUser _currentUser;
+    private string _userRole;
     private CurrentEmployeeDto _currentEmployee;
+    private CurrentUser _currentUser;
     private bool _isAuthenticated;
 
     protected override async Task OnInitializedAsync()
@@ -22,29 +22,25 @@ public partial class MainLayout
 
     private async Task HandleAuthenticationAndNavigation() {
         var authState = await AuthStateService.GetAuthenticationStateAsync();
+        _currentUser = await AuthStateService.GetCurrentUserAsync();
         _isAuthenticated = authState.User.Identity.IsAuthenticated;
 
         if (_isAuthenticated) {
-            var result = await CurrentUserInfoService.GetCurrentUserRoleAsync();
-            var role = result
-                .ToUpper()
-                .Replace('_', ' ');
+            _userRole = await CurrentUserInfoService.GetCurrentUserRoleAsync();
 
             if (_currentUser.UserName != ADMIN_EMAIL) {
                 _currentEmployee = await CurrentUserInfoService.GetCurrentEmployeeIdAsync(_currentUser.UserName);
             }
 
-            if (Enum.TryParse(role, out Roles parsedRole)) {
-                string navigationUrl = BuildNavigationUrl(parsedRole);
-                NavigationManager.NavigateTo(navigationUrl, true);
+            if (Enum.TryParse(_userRole, out Roles parsedRole)) {
+                var navigationUrl = BuildNavigationUrl(parsedRole);
+                NavigationManager.NavigateTo(navigationUrl);
             }
-        } else {
-            NavigationManager.NavigateTo(ConstantValues.LOGIN_PAGE_LONG, true);
         }
     }
 
     private string BuildNavigationUrl(Roles role) {
-        string url = $"{role.ToString().ToLower().Replace('_', '-')}";
+        var url = $"{role.ToString().ToLower().Replace('_', '-')}";
         
         if (role == Roles.SENIOR_DEVELOPER || role == Roles.JUNIOR_DEVELOPER) {
             url += $"/{_currentEmployee.Id}";
