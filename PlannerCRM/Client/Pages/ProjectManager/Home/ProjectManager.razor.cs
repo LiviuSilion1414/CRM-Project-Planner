1e6f3baca010e893ba9d0882a9f61fdb7fc56c32
liviu-silion <liviu.silion@kappas.it>
2023-08-20 16:39:58 +0200

    Add project manager home page

--- PlannerCRM/Client/Pages/ProjectManager/Home/ProjectManager.razor.cs
+++ PlannerCRM/Client/Pages/ProjectManager/Home/ProjectManager.razor.cs
@@ -2,4 +2,23 @@ namespace PlannerCRM.Client.Pages.ProjectManager.Home;
 
 public partial class ProjectManager : ComponentBase 
 {
+    [Inject] public ProjectManagerService ProjectManagerService { get; set; }
+    [Inject] public NavigationLockService NavigationUtil { get; set; }
+
+    private List<WorkOrderCostDto> _workOrders;
+    private bool _isCancelClicked;
+    private string _currentPage;
+    private int _collectionSize;
+    
+    protected override async Task OnInitializedAsync() {
+        _workOrders = await ProjectManagerService.GetPaginatedAsync();
+    }
+
+    protected override void OnInitialized() {
+        _currentPage = NavigationUtil.GetCurrentPage();
+    }
+
+    private void OnClickHideBanner() => _isCancelClicked = !_isCancelClicked;
+
+    
 }
