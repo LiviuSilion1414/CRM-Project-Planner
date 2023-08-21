be6f8f0f5db4303274d443015312fce817194fbb
liviu-silion <silionliviu001@gmail.com>
2023-08-21 16:45:21 +0200

    Add clients table to db

--- PlannerCRM/Server/DataAccess/AppDbContext.cs
+++ PlannerCRM/Server/DataAccess/AppDbContext.cs
@@ -23,6 +23,7 @@ public class AppDbContext: IdentityDbContext
     public DbSet<WorkOrder> WorkOrders { get; set; }
     public DbSet<Activity> Activities { get; set; }
     public DbSet<Employee> Employees { get; set; }
+    public DbSet<FirmClient> Clients { get; set; }
     public DbSet<EmployeeActivity> EmployeeActivity { get; set; }
     public DbSet<WorkTimeRecord> WorkTimeRecords { get; set; }
 }
