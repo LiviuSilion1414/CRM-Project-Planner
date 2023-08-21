df6aab4e0cde0f4d09c66ea132d731b658b7ac5f
liviu-silion <silionliviu001@gmail.com>
2023-08-21 19:37:59 +0200

    Add separate list for clients with crud operations

--- PlannerCRM/Client/Pages/OperationManager/Home/OperationManager.razor.cs
+++ PlannerCRM/Client/Pages/OperationManager/Home/OperationManager.razor.cs
@@ -6,6 +6,7 @@ public partial class OperationManager : ComponentBase
     [Inject] public OperationManagerCrudService OperationManagerService { get; set; }
 
     private List<WorkOrderViewDto> _workOrders = new();
+    private List<ClientViewDto> _clients = new();
     private WorkOrderViewDto _currentWorkOrder = new();
     
     private bool _trIsClicked;
@@ -13,10 +14,16 @@ public partial class OperationManager : ComponentBase
     private bool _isCreateWorkOrderClicked;
     private bool _isEditWorkOrderClicked;
     private bool _isDeleteWorkOrderClicked;
+
+    private bool _isCreateClientClicked;
+    private bool _isEditClientClicked;
+    private bool _isDeleteClientClicked;
     
     private bool _isCreateActivityClicked;
+    private bool _isShowClientsClicked;
 
     private int _workOrderId; 
+    private int _clientId; 
 
     private int _collectionSize;
 
@@ -27,6 +34,11 @@ public partial class OperationManager : ComponentBase
     
     public async Task HandlePaginate(int limit, int offset) =>
         _workOrders = await OperationManagerService.GetCollectionPaginated(limit, offset);
+
+    public async Task OnClickShowClients() {
+        _isShowClientsClicked = !_isShowClientsClicked;
+        _clients = await OperationManagerService.GetClientsPaginated();
+    }
 
     private void OnClickTableRow(int workorderId) {
         _trIsClicked = !_trIsClicked;
@@ -47,5 +59,18 @@ public partial class OperationManager : ComponentBase
     public void OnClickDelete(int id) {
         _isDeleteWorkOrderClicked = !_isDeleteWorkOrderClicked;
         _workOrderId = id;
+    }
+
+    private void OnClickAddClient() =>
+       _isCreateClientClicked = !_isCreateClientClicked;
+
+    private void OnClickEditClient(int id) {
+        _isEditWorkOrderClicked = !_isEditWorkOrderClicked;
+        _clientId = id;
+    }
+
+    public void OnClickDeleteClient(int id) {
+        _isDeleteClientClicked = !_isDeleteClientClicked;
+        _clientId = id;
     }
 }
