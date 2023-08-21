a451a618fe67780d04ff6575e98040257b0dc8f8
liviu-silion <silionliviu001@gmail.com>
2023-08-21 18:02:16 +0200

    Add edit form client

---
+++ PlannerCRM/Client/Pages/OperationManager/Edit/FirmClient/ModalEditClient.razor.cs
@@ -0,0 +1,29 @@
+namespace PlannerCRM.Client.Pages.OperationManager.Edit.FirmClient;
+
+[Authorize(Roles = nameof(Roles.OPERATION_MANAGER))]
+public partial class ModalAddClient : ComponentBase
+{
+    [Parameter] public string Title { get; set; }
+    [Parameter] public int Id { get; set; }
+
+    [Inject] public OperationManagerCrudService OperationManagerService { get; set; }
+    [Inject] public NavigationLockService NavigationUtil { get; set; } 
+    [Inject] public NavigationManager NavManager { get; set; } 
+    
+    private ClientFormDto _model;
+    private string _currentPage;
+
+    protected override async Task OnInitializedAsync()
+        => await OperationManagerService.GetClientForEditAsync(Id);
+
+    protected override void OnInitialized() {
+        _model = new();
+        _currentPage = NavigationUtil.GetCurrentPage();
+    }
+
+    private async Task OnClickModalConfirm(ClientFormDto returnedModel) {
+        await OperationManagerService.AddClientAsync(returnedModel);
+
+        NavManager.NavigateTo(_currentPage, true);
+    }
+}
