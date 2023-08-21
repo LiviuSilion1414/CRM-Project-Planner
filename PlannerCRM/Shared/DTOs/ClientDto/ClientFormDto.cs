dd6f1490103f13b93db34bcf54806d506fe67bd7
liviu-silion <silionliviu001@gmail.com>
2023-08-21 16:54:57 +0200

    Add validation attributes on client dto

--- PlannerCRM/Shared/DTOs/ClientDto/ClientFormDto.cs
+++ PlannerCRM/Shared/DTOs/ClientDto/ClientFormDto.cs
@@ -3,7 +3,11 @@ namespace PlannerCRM.Shared.Dtos.ClientDto;
 public class ClientFormDto
 {
     public int Id { get; set; }
+    
+    [Required(ErrorMessage = """ Campo "Nome" richiesto """)]
     public string Name { get; set; }
+
+    [Required(ErrorMessage = """ Campo "Partita IVA" richiesto """)]
     public string VatNumber { get; set; }
 
     public int WorkOrderId { get; set; }
