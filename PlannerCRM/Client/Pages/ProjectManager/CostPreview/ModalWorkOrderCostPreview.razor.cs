5d8b68900f1ec23397888f09da6afd9534e266a7
liviu-silion <silionliviu001@gmail.com>
2023-08-30 12:04:49 +0200

    Add project manager pages

--- PlannerCRM/Client/Pages/ProjectManager/CostPreview/ModalWorkOrderCostPreview.razor.cs
+++ PlannerCRM/Client/Pages/ProjectManager/CostPreview/ModalWorkOrderCostPreview.razor.cs
@@ -1,6 +1,42 @@
 namespace PlannerCRM.Client.Pages.ProjectManager.CostPreview;
 
-public partial class CostPreview : ComponentBase
+public partial class ModalWorkOrderCostPreview : ComponentBase
 {
-    
+    [Parameter] public int WorkOrderId { get; set; }
+    [Parameter] public string Title { get; set; }
+
+    [Inject] public ProjectManagerService ProjectManagerService { get; set; }
+    [Inject] public OperationManagerCrudService OperationManagerService { get; set; }
+    [Inject] public NavigationLockService NavigationUtil { get; set; }
+    [Inject] public NavigationManager NavigationManager { get; set; }
+
+    private WorkOrderCostDto _invoice = new();
+    private WorkOrderViewDto _workOrder = new();
+    private ClientViewDto _client = new();
+    private bool _isCancelClicked = false;
+    private bool _isInvoiceClicked = false; 
+    private string _currentPage;
+    private int _collectionSize;
+
+    private readonly bool _isDisabled = true;
+
+    protected override async Task OnInitializedAsync() {
+        await ProjectManagerService.AddInvoiceAsync(WorkOrderId);
+        
+        _invoice = await ProjectManagerService.GetInvoiceAsync(WorkOrderId);
+        _client = await OperationManagerService.GetClientForViewAsync(_invoice.ClientId);
+        _workOrder = await OperationManagerService.GetWorkOrderForViewAsync(WorkOrderId);
+    }
+
+    protected override void OnInitialized()
+        => _currentPage = NavigationUtil.GetCurrentPage();
+
+    private void OnClickModalCancel() {
+        _isCancelClicked = !_isCancelClicked;
+        NavigationManager.NavigateTo(_currentPage);
+    }
+
+    private void OnClickIssueInvoice() {
+        _isInvoiceClicked = !_isInvoiceClicked;
+    }
 }
--- PlannerCRM/Client/Pages/ProjectManager/Home/ProjectManager.razor.cs
+++ PlannerCRM/Client/Pages/ProjectManager/Home/ProjectManager.razor.cs
@@ -5,20 +5,37 @@ public partial class ProjectManager : ComponentBase
     [Inject] public ProjectManagerService ProjectManagerService { get; set; }
     [Inject] public NavigationLockService NavigationUtil { get; set; }
 
-    private List<WorkOrderCostDto> _workOrders;
-    private bool _isCancelClicked;
+    private List<WorkOrderViewDto> _workOrders = new();
+    private List<ClientViewDto> _clients = new();
+    private bool _isCancelClicked; 
+    private bool _isInvoiceClicked; 
     private string _currentPage;
     private int _collectionSize;
+    private int _workOrderId;
     
     protected override async Task OnInitializedAsync() {
-        _workOrders = await ProjectManagerService.GetPaginatedAsync();
+       await FetchDataAsync();
+    }
+
+    private async Task FetchDataAsync(int limit = 0, int offset = 5) {
+        _workOrders = await ProjectManagerService.GetPaginatedAsync(limit, offset);
+
+        foreach (var wo in _workOrders) {
+            var client = await ProjectManagerService.GetClientForViewAsync(wo.ClientId);
+            _clients.Add(client);
+        }
     }
 
     protected override void OnInitialized() {
         _currentPage = NavigationUtil.GetCurrentPage();
     }
 
-    private void OnClickHideBanner() => _isCancelClicked = !_isCancelClicked;
+    private void ViewReport(int workOrderId) {
+        _isInvoiceClicked = !_isInvoiceClicked;
+        _workOrderId = workOrderId;
+    }
 
-    
+    private async Task HandlePaginate(int limit, int offset) {
+       await FetchDataAsync(limit, offset);
+    }        
 }
