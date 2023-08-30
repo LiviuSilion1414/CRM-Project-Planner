104626e9f6fcb8774be9a4e9d1f468a83c8d6079
liviu-silion <silionliviu001@gmail.com>
2023-08-30 12:18:10 +0200

    Apply new attribute

--- PlannerCRM/Shared/DTOs/EmployeeDto/Forms/EmployeeLoginDTO.cs
+++ PlannerCRM/Shared/DTOs/EmployeeDto/Forms/EmployeeLoginDTO.cs
@@ -8,7 +8,7 @@ public class EmployeeLoginDto
     [Required(ErrorMessage = """Campo "Email" richiesto""")]
     public string Email { get; set; }
 
-    [Required(ErrorMessage = """Campo "Password" richiesto""")]
+    [StringIsNotNullOrEmpty(ErrorMessage = """Campo "Password" richiesto""")]
     [PasswordValidator(PASS_MIN_LENGTH, PASS_MAX_LENGTH, 
         ErrorMessage="La password deve avere tra 8 e 16 caratteri.")]
     public string Password { get; set; }
