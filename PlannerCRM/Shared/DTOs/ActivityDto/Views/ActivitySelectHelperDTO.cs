f98ada9aec94ff6fac34bb7b88a5c5feb22ce0ae
liviu-silion <silionliviu001@gmail.com>
2023-09-01 01:39:58 +0200

    Remove useless class

--- PlannerCRM/Shared/DTOs/ActivityDto/Views/ActivitySelectHelperDTO.cs
+++
@@ -1,7 +0,0 @@
-namespace PlannerCRM.Shared.DTOs.ActivityDto.Views;
-
-public class ActivitySelectHelperDto
-{ 
-    public WorkOrderSelectDto WorkOrderDto { get; set; }
-    public EmployeeSelectDto EmployeeDto { get; set; }
-}
