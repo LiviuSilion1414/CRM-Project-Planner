a48e29afc4f2252e20d9633d5c544ec47982be48
liviu-silion <silionliviu001@gmail.com>
2023-09-13 11:41:12 +0200

    Create UpdateRowSourceException.cs

---
+++ PlannerCRM/Shared/CustomExceptions/UpdateRowSourceException.cs
@@ -0,0 +1,10 @@
+namespace PlannerCRM.Shared.CustomExceptions;
+
+public class UpdateRowSourceException : Exception
+{
+    
+    public UpdateRowSourceException(string message) 
+        : base(message) 
+    { }
+
+}
