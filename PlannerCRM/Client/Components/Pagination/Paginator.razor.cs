671e707690f90570a6406df273836cdbac351257
liviu-silion <silionliviu001@gmail.com>
2023-09-15 14:32:55 +0200

    Fix paginator next method

--- PlannerCRM/Client/Components/Pagination/Paginator.razor.cs
+++ PlannerCRM/Client/Components/Pagination/Paginator.razor.cs
@@ -5,19 +5,26 @@ public partial class Paginator : ComponentBase
     [Parameter] public int CollectionSize { get; set; }
     [Parameter] public EventCallback<(int, int)> Paginate { get; set; }
 
+    private List<int> _pages;
     private readonly int _elementsToTake = 5;
     private int _elementsToSkip;
-    private int _totalPageNumbers;
     private int _pageNumber;
-    private double _result; 
+    private int _slices;
     
     protected override void OnInitialized() {
         _elementsToSkip = 0;
-        _pageNumber = 1;
-        _result = CollectionSize % _elementsToTake;
-        _totalPageNumbers = _elementsToTake > CollectionSize
-            ? 1
-            : ((int) Math.Ceiling(_result));
+        _slices = CollectionSize / _elementsToTake;
+        _pages = Enumerable
+            .Range(1, _slices)
+            .ToList();
+        _pageNumber = _pages
+            .First();
+    }
+
+    private string SetClass(int pageNumber) {
+        return pageNumber == _pageNumber
+            ? CssClass.Selected
+            : CssClass.Empty;
     }
 
     private async Task Previous() {
@@ -33,7 +40,7 @@ public partial class Paginator : ComponentBase
     }
 
     private async Task Next() {
-        if (_elementsToSkip <= (_totalPageNumbers + _elementsToTake)) {
+        if (_elementsToSkip < CollectionSize && (_elementsToSkip + _elementsToTake < CollectionSize)) {
             _elementsToSkip += _elementsToTake;
             _pageNumber++; 
         }
