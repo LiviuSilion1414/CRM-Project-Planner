5a99b69e641883ce27b5a2d9333e5ea38a37ce08
liviu-silion <silionliviu001@gmail.com>
2023-10-22 11:09:21 +0200

    Create Exception Handler Middleware

---
+++ PlannerCRM/Server/Middlewares/AbstractExceptionHandlerMiddleware.cs
@@ -0,0 +1,30 @@
+using System.Net;
+using System.Reflection;
+using Serilog;
+
+namespace PlannerCRM.Server.Middlewares;
+
+public abstract class AbstractExceptionHandlerMiddleware : IMiddleware
+{
+    private static readonly Serilog.ILogger Logger = Log.ForContext(MethodBase.GetCurrentMethod()?.DeclaringType);
+
+    public abstract (HttpStatusCode code, string message) GetResponse(Exception exception);
+
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try 
+        {
+            await next(context);
+        } 
+        catch (Exception exc) 
+        {
+            var response = context.Response;
+            response.ContentType = "application/json";
+            Logger.Error(context.Request.Method, $"Error during executing {context}", context.Request.Path.Value);
+            
+            var (status, message) = GetResponse(exc);
+            response.StatusCode = (int) status;
+            await response.WriteAsync(message);
+        }
+    }
+}
---
+++ PlannerCRM/Server/Middlewares/GlobalExceptionHandlerMiddleware.cs
@@ -0,0 +1,19 @@
+using System.Text.Json;
+
+namespace PlannerCRM.Server.Middlewares;
+
+public class GlobalExceptionHandlerMiddleware : AbstractExceptionHandlerMiddleware
+{
+    public override (HttpStatusCode code, string message) GetResponse(Exception exception)
+    {
+        var code = exception switch {
+            KeyNotFoundException => HttpStatusCode.NotFound,
+            DuplicateElementException => HttpStatusCode.Conflict,
+            UnauthorizedAccessException => HttpStatusCode.Unauthorized,
+            ArgumentNullException => HttpStatusCode.BadRequest,
+            _ => HttpStatusCode.InternalServerError,
+        };
+
+        return (code, JsonSerializer.Serialize(exception.Message));
+    }
+}
