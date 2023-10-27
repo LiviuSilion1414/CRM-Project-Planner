03769e53adc17ecab5ee08396d5a5cb7b4326538
liviu-silion <silionliviu001@gmail.com>
2023-10-27 16:50:31 +0200

    Add floating profile menu

---
+++ PlannerCRM/Client/Components/ProfileMenu/ProfileModal/ExtendableProfileModal.razor.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Http;
+
+namespace PlannerCRM.Client.Components.ProfileMenu.ProfileModal;
+
+public partial class ExtendableProfileModal : ComponentBase
+{
+    [Inject] public NavigationManager NavManager { get; set; }
+    [Inject] public AuthenticationStateService AuthStateService { get; set; }
+    [Inject] public LoginService LoginService { get; set; }
+
+    private CurrentUser _currentUser = new();
+    private bool _isMenuSwitched = false;
+
+    protected override async Task OnInitializedAsync()
+        => _currentUser = await AuthStateService.GetCurrentUserAsync();
+
+    private void SwitchMenu() {
+        _isMenuSwitched = !_isMenuSwitched;
+    }
+
+    public async Task OnClickLogout() {
+        await LoginService.LogoutAsync();
+        NavManager.NavigateTo(ConstantValues.LOGIN_PAGE_LONG, true);
+    }    
+}
