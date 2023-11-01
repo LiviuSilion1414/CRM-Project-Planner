ad857295b80bd174b59be0e0f1abe2098c56fc90
liviu-silion <silionliviu001@gmail.com>
2023-11-01 12:24:41 +0100

    Update CssClass.cs

--- PlannerCRM/Shared/Models/CssClass.cs
+++ PlannerCRM/Shared/Models/CssClass.cs
@@ -5,4 +5,8 @@ public static class CssClass
     public static string Active = "active";
     public static string Selected = "selected";
     public static string Empty = string.Empty;
+    public static string FormControl = "form-control";
+    public static string Span = "span";
+    public static string Down = "down";
+    public static string Up = "up";
 }
