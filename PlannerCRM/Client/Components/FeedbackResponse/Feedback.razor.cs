5bfb4bb9fd0b690619ef92f7792f61b2fd6119db
liviu-silion <silionliviu001@gmail.com>
2023-11-21 18:13:58 +0100

    Add eventcallback to close banner

--- PlannerCRM/Client/Components/FeedbackResponse/Feedback.razor.cs
+++ PlannerCRM/Client/Components/FeedbackResponse/Feedback.razor.cs
@@ -4,9 +4,8 @@ public partial class Feedback : ComponentBase
 {
     [Parameter] public string Severity { get; set; }
     [Parameter] public string Message { get; set; }
+    [Parameter] public EventCallback<bool> OnClickCancel { get; set; }
 
-    private bool _isHidden = false;
-
-    private void HideBanner()
-        => _isHidden = true;
+    private async Task HideBanner()
+        => await OnClickCancel.InvokeAsync(false);
 }
