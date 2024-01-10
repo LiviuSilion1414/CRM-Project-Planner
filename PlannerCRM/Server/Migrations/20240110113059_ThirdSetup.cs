64f501e5e8de3298c1fb3bc3346b514d3b8fde20
liviusilion-dev <silionliviu001@gmail.com>
2024-01-10 12:31:50 +0100

    Apply migrations

---
+++ PlannerCRM/Server/Migrations/20240110113059_ThirdSetup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlannerCRM.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class ThirdSetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProfilePictureId",
+                table: "Employees",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilePictureId",
+                table: "Employees");
+        }
+    }
+}
