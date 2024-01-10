7b4cadf47e38a1a4afbfff156eecb27aec889d18
liviusilion-dev <silionliviu001@gmail.com>
2024-01-10 15:12:44 +0100

    Update convert image method

--- PlannerCRM/Client/Utilities/Converter/Base64Converter.cs
+++ PlannerCRM/Client/Utilities/Converter/Base64Converter.cs
@@ -4,7 +4,7 @@ namespace PlannerCRM.Client.Utilities.Converter;
 
 public class Base64Converter
 {
-    public static async Task<(string, string)> ConvertImage(InputFileChangeEventArgs args, long allowedSize) {
+    public static async Task<(string thumbnail, string imageType, string fileName)> ConvertImageAsync(InputFileChangeEventArgs args, long allowedSize) {
         var imgFile = args.File;
         var buffers = new byte[imgFile.Size];
 
@@ -14,6 +14,6 @@ public class Base64Converter
         var fileName = imgFile.Name;        
         var thumbnail = $"data:{imageType};base64,{Convert.ToBase64String(buffers)}";
 
-        return (thumbnail, imageType);
+        return (thumbnail, imageType, fileName);
     }
 }
