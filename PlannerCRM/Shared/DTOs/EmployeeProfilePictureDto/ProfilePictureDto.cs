75edba60504ca89e9e182b55155637e54bed3798
liviusilion-dev <silionliviu001@gmail.com>
2024-01-10 12:36:33 +0100

    Add profile picture dto

---
+++ PlannerCRM/Shared/DTOs/EmployeeProfilePictureDto/ProfilePictureDto.cs
@@ -0,0 +1,16 @@
+namespace PlannerCRM.Shared.DTOs.EmployeeProfilePictureDto;
+
+public class ProfilePictureDto
+{
+    public int Id { get; set; }
+
+    public int EmployeeId { get; set; }
+
+    public string ImageType { get; set; }
+
+    public string? Thumbnail { get; set; }
+
+    public string? ImageUrl { get; set; }
+
+    public virtual Employee? EmployeeInfo { get; set; }
+}
