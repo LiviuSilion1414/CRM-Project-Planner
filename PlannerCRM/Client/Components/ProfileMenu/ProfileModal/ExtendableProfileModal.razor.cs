0ef2c3d11156cab9a3ebe5bfa591d139a19e2d9c
liviusilion-dev <silionliviu001@gmail.com>
2024-01-12 12:39:47 +0100

    Add pic thumbnail on dashboard navbar

--- PlannerCRM/Client/Components/ProfileMenu/ProfileModal/ExtendableProfileModal.razor.cs
+++ PlannerCRM/Client/Components/ProfileMenu/ProfileModal/ExtendableProfileModal.razor.cs
@@ -6,20 +6,21 @@ public partial class ExtendableProfileModal : ComponentBase
     [Inject] public AuthenticationStateService AuthStateService { get; set; }
     [Inject] public LoginService LoginService { get; set; }
 
-    private CurrentUser _currentUser = new();
+    private CurrentUser _currentUser;
     private bool _isMenuSwitched = false;
 
     protected override async Task OnInitializedAsync()
         => _currentUser = await AuthStateService.GetCurrentUserAsync();
 
-    private void SwitchMenu() {
-        _isMenuSwitched = !_isMenuSwitched;
+    protected override void OnInitialized() {
+        _currentUser= new() {
+            ProfilePicture = new(),
+            Claims = new()
+        };
     }
 
-    private string SetMenuArrow() {
-        return !_isMenuSwitched
-            ? CssClass.Down
-            : CssClass.Up;
+    private void SwitchMenu() {
+        _isMenuSwitched = !_isMenuSwitched;
     }
 
     private void NavigateToProfileSettings() {
