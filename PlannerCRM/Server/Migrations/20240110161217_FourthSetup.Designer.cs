d95f18e21c6f33b3c5d5c793947de42492b0c5fb
liviusilion-dev <silionliviu001@gmail.com>
2024-01-12 12:39:00 +0100

    Add migrations with minor updates

--- PlannerCRM/Server/Migrations/20240110161217_FourthSetup.Designer.cs
+++ PlannerCRM/Server/Migrations/20240110161217_FourthSetup.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PlannerCRM.Server.DataAccess;
@@ -11,9 +12,11 @@ using PlannerCRM.Server.DataAccess;
 namespace PlannerCRM.Server.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240110161217_FourthSetup")]
+    partial class FourthSetup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -411,8 +414,10 @@ namespace PlannerCRM.Server.Migrations
 
                     NpgsqlPropertyBuilderExtensions.UseIdentityByDefaultColumn(b.Property<int>("Id"));
 
-                    b.Property<string>("EmployeeId")
-                        .IsRequired()
+                    b.Property<int>("EmployeeId")
+                        .HasColumnType("integer");
+
+                    b.Property<string>("EmployeeInfoId")
                         .HasColumnType("text");
 
                     b.Property<string>("ImageType")
@@ -424,7 +429,7 @@ namespace PlannerCRM.Server.Migrations
 
                     b.HasKey("Id");
 
-                    b.HasIndex("EmployeeId");
+                    b.HasIndex("EmployeeInfoId");
 
                     b.ToTable("ProfilePictures");
                 });
@@ -714,9 +719,7 @@ namespace PlannerCRM.Server.Migrations
                 {
                     b.HasOne("PlannerCRM.Server.Models.Employee", "EmployeeInfo")
                         .WithMany()
-                        .HasForeignKey("EmployeeId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
+                        .HasForeignKey("EmployeeInfoId");
 
                     b.Navigation("EmployeeInfo");
                 });
---
+++ PlannerCRM/Server/Migrations/20240110164729_FifthSetup.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlannerCRM.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class FifthSetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProfilePictures_Employees_EmployeeInfoId",
+                table: "ProfilePictures");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProfilePictures_EmployeeInfoId",
+                table: "ProfilePictures");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeInfoId",
+                table: "ProfilePictures");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EmployeeId",
+                table: "ProfilePictures",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProfilePictures_EmployeeId",
+                table: "ProfilePictures",
+                column: "EmployeeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProfilePictures_Employees_EmployeeId",
+                table: "ProfilePictures",
+                column: "EmployeeId",
+                principalTable: "Employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProfilePictures_Employees_EmployeeId",
+                table: "ProfilePictures");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProfilePictures_EmployeeId",
+                table: "ProfilePictures");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EmployeeId",
+                table: "ProfilePictures",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmployeeInfoId",
+                table: "ProfilePictures",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProfilePictures_EmployeeInfoId",
+                table: "ProfilePictures",
+                column: "EmployeeInfoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProfilePictures_Employees_EmployeeInfoId",
+                table: "ProfilePictures",
+                column: "EmployeeInfoId",
+                principalTable: "Employees",
+                principalColumn: "Id");
+        }
+    }
+}
