47b4335d2e0eac9f692d2ee69fde3048f89dc119
liviusilion-dev <silionliviu001@gmail.com>
2024-01-15 12:20:21 +0100

    Link the table the original Identity entity maps to

--- PlannerCRM/Server/DataAccess/AppDbContext.cs
+++ PlannerCRM/Server/DataAccess/AppDbContext.cs
@@ -8,16 +8,14 @@ public class AppDbContext: IdentityDbContext<IdentityUser>
 
     protected override void OnModelCreating(ModelBuilder modelBuilder) {
         base.OnModelCreating(modelBuilder);
-
-        modelBuilder
-            .Entity<Employee>()
-            .Property(em => em.Id)
-            .ValueGeneratedOnAdd();
-
-        modelBuilder
-            .Entity<EmployeeSalary>()
-            .Property(em => em.Id)
-            .ValueGeneratedOnAdd();
+        
+        modelBuilder.Entity<IdentityUser>().ToTable(nameof(Employees));
+        modelBuilder.Entity<IdentityRole>().ToTable(nameof(EmployeeRoles));
+        modelBuilder.Entity<IdentityUserRole<string>>().ToTable(nameof(EmployeeWithRoles));
+        modelBuilder.Entity<IdentityRoleClaim<int>>().ToTable(nameof(EmployeeRoleClaims));
+        modelBuilder.Entity<IdentityUserClaim<string>>().ToTable(nameof(EmployeeClaims));
+        modelBuilder.Entity<IdentityUserLogin<string>>().ToTable(nameof(EmployeeLogins));
+        modelBuilder.Entity<IdentityUserToken<string>>().ToTable(nameof(EmployeeTokens));
 
         modelBuilder.Entity<EmployeeActivity>()
             .HasOne(e => e.Employee)
@@ -30,13 +28,22 @@ public class AppDbContext: IdentityDbContext<IdentityUser>
             .HasForeignKey(ai => ai.ActivityId);
     }
     
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<EmployeeRole> EmployeeRoles { get; set; }
+    public DbSet<EmployeeWithRole> EmployeeWithRoles { get; set; }
+    public DbSet<EmployeeRoleClaim> EmployeeRoleClaims { get; set; }
+    public DbSet<EmployeeClaim> EmployeeClaims { get; set; }
+    public DbSet<EmployeeLogin> EmployeeLogins { get; set; }
+    public DbSet<EmployeeToken> EmployeeTokens { get; set; }
+
+    public DbSet<EmployeeProfilePicture> ProfilePictures { get; set; 
+    }
+    public DbSet<FirmClient> Clients { get; set; }
     public DbSet<WorkOrder> WorkOrders { get; set; }
     public DbSet<Activity> Activities { get; set; }
-    public DbSet<Employee> Employees { get; set; }
-    public DbSet<EmployeeProfilePicture> ProfilePictures { get; set; }
-    public DbSet<FirmClient> Clients { get; set; }
     public DbSet<EmployeeActivity> EmployeeActivity { get; set; }
-    public DbSet<WorkTimeRecord> WorkTimeRecords { get; set; }
+    
     public DbSet<WorkOrderCost> WorkOrderCosts { get; set; }
+    public DbSet<WorkTimeRecord> WorkTimeRecords { get; set; }
     public DbSet<ClientWorkOrder> ClientWorkOrders { get; set; }
 }
