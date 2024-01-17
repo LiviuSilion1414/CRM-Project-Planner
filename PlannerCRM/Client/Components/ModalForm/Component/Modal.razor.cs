492a60ee0df526614469f4f6b77b82063a676458
liviusilion-dev <silionliviu001@gmail.com>
2024-01-17 09:36:17 +0100

    Remove nullable type

--- PlannerCRM/Client/Components/ModalForm/Component/Modal.razor.cs
+++ PlannerCRM/Client/Components/ModalForm/Component/Modal.razor.cs
@@ -6,7 +6,7 @@ public partial class Modal : ComponentBase
 {
     [Parameter] public string Size { get; set; } = ModalSize.DEFAULT;
     [Parameter] public string Title { get; set; } = Titles.DEFAULT_MODAL_TITLE;
-    [Parameter] public Dictionary<string, object>? AdditionalAttributes { get; set; }
+    [Parameter] public Dictionary<string, object> AdditionalAttributes { get; set; }
     
     [Parameter] public RenderFragment Header { get; set; }
     [Parameter] public RenderFragment Body { get; set; }
