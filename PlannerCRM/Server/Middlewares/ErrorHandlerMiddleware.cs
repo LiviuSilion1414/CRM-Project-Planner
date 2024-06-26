a708c4e450c13db29ac645e930f2085971d5b732
liviusilion-dev <silionliviu001@gmail.com>
2024-06-26 23:59:31 +0200

    Add exception handler middleware

---
+++ PlannerCRM/Server/Middlewares/ErrorHandlerMiddleware.cs
@@ -0,0 +1,18 @@
+
+namespace PlannerCRM.Server.Middlewares;
+
+public class ExceptionHandlerMiddleware : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch
+        {
+            await context.Response
+                .WriteAsync("An error occurred while processing your request. Try again later.");
+        }
+    }
+}
