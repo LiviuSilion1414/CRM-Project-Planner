ca7a56c85ed1d77828464b572ca38915e8cc3c3e
liviusilion-dev <silionliviu001@gmail.com>
2024-06-28 01:24:42 +0200

    Fix worktimeorder controller

--- PlannerCRM/Server/Controllers/WorkTimeRecordController.cs
+++ PlannerCRM/Server/Controllers/WorkTimeRecordController.cs
@@ -18,94 +18,30 @@ public class WorkTimeRecordController : ControllerBase
 
     [HttpPost("add")]
     public async Task<IActionResult> AddWorkTimeRecordAsync(WorkTimeRecordFormDto dto) {
-        try {
-            await _repo.AddAsync(dto);
-
-            return Ok(SuccessfulCrudFeedBack.WORKTIMERECORD_ADD);
-        } catch (NullReferenceException exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return NotFound(exc.Message);
-        } catch (ArgumentNullException exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return BadRequest(exc.Message);
-        } catch (DuplicateElementException exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return BadRequest(exc.Message);
-        } catch (DbUpdateException exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return BadRequest(exc.Message);
-        } catch (Exception exc) {
-            return StatusCode(StatusCodes.Status503ServiceUnavailable, exc.Message);
-        }
+        await _repo.AddAsync(dto);
+        return Ok(SuccessfulCrudFeedBack.WORKTIMERECORD_ADD);
     }
     
     [Authorize(Roles = nameof(Roles.SENIOR_DEVELOPER))]
     [Authorize(Roles = nameof(Roles.JUNIOR_DEVELOPER))]
     [HttpPut("edit")]
     public async Task<IActionResult> EditWorkTimeRecordAsync(WorkTimeRecordFormDto dto) {
-        try {
-            await _repo.EditAsync(dto);
-
-            return Ok(SuccessfulCrudFeedBack.WORKTIMERECORD_EDIT);
-        } catch (NullReferenceException exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return BadRequest(exc.Message);
-        } catch (ArgumentNullException exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return BadRequest(exc.Message);
-        } catch (KeyNotFoundException exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return NotFound(exc.Message);
-        } catch (DuplicateElementException exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return BadRequest(exc.Message);
-        } catch (DbUpdateException exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return BadRequest(exc.Message);
-        } catch (Exception exc) {
-            return StatusCode(StatusCodes.Status503ServiceUnavailable, exc.Message);
-        }
+        await _repo.EditAsync(dto);
+        return Ok();
     }
     
     [HttpGet("get/{workOrderId}/{activityId}/{employeeId}")]
     public async Task<WorkTimeRecordViewDto> GetWorkTimeRecordAsync(int workOrderId, int activityId, string employeeId) {
-        try {
-            return await _repo.GetAsync(workOrderId, activityId, employeeId);
-        } catch (KeyNotFoundException exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return new();
-        }
+        return await _repo.GetAsync(workOrderId, activityId, employeeId);
     }
     
     [HttpGet("get/paginated/{limit}/{offset}")]
     public async Task<List<WorkTimeRecordViewDto>> GetPaginatedWorkTimeRecordsAsync(int limit, int offset) {
-        try {
-            return await _repo.GetPaginatedWorkTimeRecordsAsync(limit, offset);
-        } catch (Exception exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return new();
-        }
+        return await _repo.GetPaginatedWorkTimeRecordsAsync(limit, offset);
     }
     
     [HttpGet("get/by/employee/{employeeId}")]
     public async Task<WorkTimeRecordViewDto> GetAllWorkTimeRecordsByEmployeeIdAsync(string employeeId) {
-        try {
-            return await _repo.GetAllWorkTimeRecordsByEmployeeIdAsync(employeeId);
-        } catch (Exception exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return new();
-        }
+        return await _repo.GetAllWorkTimeRecordsByEmployeeIdAsync(employeeId);
     }
 }
