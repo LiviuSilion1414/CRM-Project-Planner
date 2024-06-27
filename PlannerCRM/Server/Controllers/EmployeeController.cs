dcca46b90a905ddeaa763eec266d967362cbed29
liviusilion-dev <silionliviu001@gmail.com>
2024-06-28 01:20:04 +0200

    Fix employee controller

--- PlannerCRM/Server/Controllers/EmployeeController.cs
+++ PlannerCRM/Server/Controllers/EmployeeController.cs
@@ -22,227 +22,82 @@ public class EmployeeController : ControllerBase
     [Authorize(Roles = nameof(Roles.ACCOUNT_MANAGER))]
     [HttpPost("add")]
     public async Task<IActionResult> AddEmployeeAsync(EmployeeFormDto dto) {
-        try {
-            await _repo.AddAsync(dto);
 
-            return Ok(SuccessfulCrudFeedBack.USER_ADD);
-        } catch (NullReferenceException exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return BadRequest(exc.Message);
-        } catch (ArgumentNullException exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return BadRequest(exc.Message);
-        } catch (DuplicateElementException exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return BadRequest(exc.Message);
-        } catch (DbUpdateException exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return StatusCode(StatusCodes.Status500InternalServerError);
-        } catch (Exception exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return StatusCode(StatusCodes.Status503ServiceUnavailable);
-        }
+        await _repo.AddAsync(dto);
+        return Ok(SuccessfulCrudFeedBack.USER_ADD);
     }
 
     [Authorize]
     [HttpPut("edit")]
     public async Task<IActionResult> EditEmployeeAsync(EmployeeFormDto dto) {
-        try {
-            await _repo.EditAsync(dto);
-
-            return Ok(SuccessfulCrudFeedBack.USER_EDIT);
-        } catch (NullReferenceException exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return NotFound(exc.Message);
-        } catch (ArgumentNullException exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return BadRequest(exc.Message);
-        } catch (KeyNotFoundException exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return NotFound(exc.Message);
-        } catch (DbUpdateException exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return StatusCode(StatusCodes.Status500InternalServerError);
-        } catch (Exception exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return StatusCode(StatusCodes.Status503ServiceUnavailable);
-        }
+        return Ok(SuccessfulCrudFeedBack.USER_EDIT);
     }
 
     [Authorize(Roles = nameof(Roles.ACCOUNT_MANAGER))]
     [HttpDelete("delete/{employeeId}")]
     public async Task<IActionResult> DeleteEmployeeAsync(string employeeId) {
-        try {
-            await _repo.DeleteAsync(employeeId);
-
-            return Ok(SuccessfulCrudFeedBack.USER_DELETE);
-        } catch (InvalidOperationException exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return BadRequest(exc.Message);
-        } catch (DbUpdateException exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return BadRequest(exc.Message);
-        } catch (Exception exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return StatusCode(StatusCodes.Status503ServiceUnavailable);
-        }
+        await _repo.DeleteAsync(employeeId);
+        return Ok();
     }
 
     [Authorize(Roles = nameof(Roles.ACCOUNT_MANAGER))]
     [HttpGet("archive/{employeeId}")]
     public async Task<IActionResult> ArchiveEmployeeAsync(string employeeId) {
-        try {
-            await _repo.ArchiveAsync(employeeId);
-
-            return Ok(SuccessfulCrudFeedBack.USER_ARCHIVE);
-        } catch (InvalidOperationException exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return BadRequest(exc.Message);
-        } catch (DbUpdateException exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return BadRequest(exc.Message);
-        } catch (Exception exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return StatusCode(StatusCodes.Status503ServiceUnavailable);
-        }
+        await _repo.ArchiveAsync(employeeId);
+        return Ok();
     }
     
     [Authorize(Roles = nameof(Roles.ACCOUNT_MANAGER))]
     [HttpGet("restore/{employeeId}")]
     public async Task<IActionResult> RestoreEmployeeAsync(string employeeId) {
-        try {
-            await _repo.RestoreAsync(employeeId);
-
-            return Ok(SuccessfulCrudFeedBack.USER_RESTORE);
-        } catch (InvalidOperationException exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return BadRequest(exc.Message);
-        } catch (DbUpdateException exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return BadRequest(exc.Message);
-        } catch (Exception exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return StatusCode(StatusCodes.Status503ServiceUnavailable);
-        }
+        await _repo.RestoreAsync(employeeId);
+        return Ok();
     }
 
     [Authorize(Roles = nameof(Roles.ACCOUNT_MANAGER))]
     [HttpGet("get/for/restore/{employeeId}")]
     public async Task<EmployeeSelectDto> GetEmployeeForRestoreByIdAsync(string employeeId) {
-        try {
-            return await _repo.GetForRestoreAsync(employeeId);
-        } catch (Exception exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return new();
-        }
+        return await _repo.GetForRestoreAsync(employeeId);
     }
 
     [HttpGet("get/for/view/{employeeId}")]
     public async Task<EmployeeViewDto> GetEmployeeForViewByIdAsync(string employeeId) {
-        try {
-            return await _repo.GetForViewByIdAsync(employeeId);
-        } catch (Exception exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return new();
-        }
+        return await _repo.GetForViewByIdAsync(employeeId);
     }
 
-    [HttpGet("get/for/edit/{employeeId}")]
-    public async Task<EmployeeFormDto> GetEmployeeForEditByIdAsync(string employeeId) {
-        try {
-            return await _repo.GetForEditByIdAsync(employeeId);
-        } catch (Exception exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return new();
-        }
+    [HttpGet("get/for/edit/{employeeId}")] 
+    public async Task<EmployeeFormDto> GetEmployeeForEditByIdAsync(string employeeId)  {
+        return await _repo.GetForEditByIdAsync(employeeId);
     }
 
     [Authorize(Roles = nameof(Roles.ACCOUNT_MANAGER))]
     [HttpGet("get/for/delete/{employeeId}")]
     public async Task<EmployeeDeleteDto> GetEmployeeForDeleteByIdAsync(string employeeId) {
-        try {
-            return await _repo.GetForDeleteByIdAsync(employeeId);
-        } catch (Exception exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return new();
-        }
+        return await _repo.GetForDeleteByIdAsync(employeeId);
     }
 
     [HttpGet("search/{email}")]
     public async Task<List<EmployeeSelectDto>> SearchEmployeeAsync(string email) {
-        try {
-            return await _repo.SearchEmployeeAsync(email);
-        } catch (Exception exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return new();
-        }
+        return await _repo.SearchEmployeeAsync(email);
     }
 
     [HttpGet("get/paginated/{limit}/{offset}")]
     public async Task<List<EmployeeViewDto>> GetPaginatedEmployeesAsync(int limit, int offset) {
-        try {
-            return await _repo.GetPaginatedEmployeesAsync(limit, offset);
-        } catch (Exception exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return new();
-        }
+        return await _repo.GetPaginatedEmployeesAsync(limit, offset);
     }
 
     [HttpGet("get/id/{email}")]
     public async Task<CurrentEmployeeDto> GetEmployeeIdAsync(string email) {
-        try {
-            return await _repo.GetEmployeeIdAsync(email);
-        } catch (Exception exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return new();
-        }
+        return await _repo.GetEmployeeIdAsync(email);
     }
 
     [HttpGet("get/id-check/{email}")]
     public async Task<string> GetUserIdCheckAsync(string email) {
-        try {
-            return (await _repo.GetEmployeeIdAsync(email)).Id;
-        } catch (Exception exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return default;
-        }
+        return (await _repo.GetEmployeeIdAsync(email)).Id;
     }
 
     [HttpGet("get/size")] 
     public async Task<int> GetEmployeesSizeAsync() {
-        try {
-            return await _repo.GetEmployeesSizeAsync();
-        } catch (Exception exc) {
-            _logger.LogError("\nError: {0} \n\nMessage: {1}", exc.StackTrace, exc.Message);
-
-            return default;
-        }
+        return await _repo.GetEmployeesSizeAsync();
     }
 }
