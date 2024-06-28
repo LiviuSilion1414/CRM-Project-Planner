90d470fbf1a38a4111cfb87c90f1f12d82ab0d59
liviusilion-dev <silionliviu001@gmail.com>
2024-06-28 12:25:10 +0200

    Add mapped queries for worktimerecord repo

---
+++ PlannerCRM/Server/Utilities/WorkTimeRecordMapper.cs
@@ -0,0 +1,63 @@
+namespace PlannerCRM.Server.Utilities;
+
+public static class WorkTimeRecordMapper
+{
+    public async static Task<WorkTimeRecord> MapToWorkTimeRecord(this WorkTimeRecordFormDto dto, AppDbContext context) {
+        return new WorkTimeRecord
+        {
+            Id = dto.Id,
+            Date = dto.Date,
+            Hours = dto.Hours
+                ?? throw new ArgumentNullException(nameof(dto.Hours), ExceptionsMessages.NULL_ARG),
+            TotalPrice = dto.TotalPrice + dto.Hours
+                ?? throw new ArgumentNullException(nameof(dto.Hours), ExceptionsMessages.NULL_ARG),
+            ActivityId = dto.ActivityId,
+            EmployeeId = dto.EmployeeId,
+            Employee = context.Employees
+                .Where(em => !em.IsDeleted && !em.IsArchived)
+                .Single(e => e.Id == dto.EmployeeId),
+            WorkOrderId = await context.WorkOrders
+                .AnyAsync(wo => !wo.IsDeleted && !wo.IsCompleted)
+                    ? dto.WorkOrderId
+                    : throw new InvalidOperationException(ExceptionsMessages.IMPOSSIBLE_ADD)
+        };
+    }
+
+    public static WorkTimeRecordViewDto MapToWorkTimeRecordViewDto(
+        this WorkTimeRecord workTimeRecord, 
+        AppDbContext context,
+        int workOrderId, 
+        int activityId, 
+        string employeeId) 
+    {
+        return new WorkTimeRecordViewDto
+        {
+            Id = workTimeRecord.Id,
+            Date = workTimeRecord.Date,
+            Hours = context.WorkTimeRecords
+                .Where(wtr =>
+                    wtr.WorkOrderId == workOrderId &&
+                    wtr.ActivityId == activityId &&
+                    wtr.EmployeeId == employeeId)
+                .Distinct()
+                .Sum(wtrSum => wtrSum.Hours),
+            TotalPrice = workTimeRecord.TotalPrice,
+            ActivityId = workTimeRecord.ActivityId,
+            WorkOrderId = workTimeRecord.WorkOrderId,
+            EmployeeId = workTimeRecord.EmployeeId
+        };
+    }
+    
+    public static WorkTimeRecordViewDto MapToWorkTimeRecordViewDto(this WorkTimeRecord workTimeRecord)
+    {
+        return new WorkTimeRecordViewDto
+        {
+            Id = workTimeRecord.Id,
+            Date = workTimeRecord.Date,
+            Hours = workTimeRecord.Hours,
+            TotalPrice = workTimeRecord.TotalPrice,
+            ActivityId = workTimeRecord.ActivityId,
+            EmployeeId = workTimeRecord.EmployeeId
+        };
+    }
+}
