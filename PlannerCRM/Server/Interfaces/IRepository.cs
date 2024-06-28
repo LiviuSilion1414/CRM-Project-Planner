d7a3e06192b25d14884c61e3aa79875248abc949
liviusilion-dev <silionliviu001@gmail.com>
2024-06-28 15:58:01 +0200

    Edit IRepository pattern

--- PlannerCRM/Server/Interfaces/IRepository.cs
+++ PlannerCRM/Server/Interfaces/IRepository.cs
@@ -1,13 +1,14 @@
 namespace PlannerCRM.Server.Interfaces;
 
-public interface IRepository<T> 
-    where T : class
+public interface IRepository<TInput, TOutput> 
+    where TInput : class
+    where TOutput : class
 {
-    public async Task AddAsync(T dto);
-    public async Task EditAsync(T dto);
-    public async Task DeleteAsync(int id);
-    public async Task<TOutput> GetForViewByIdAsync(int id);
-    public async Task<TOutput> GetForEditByIdAsync(int id);
-    public async Task<TOutput> GetForDeleteByIdAsync(int id);
-    public async Task<List<TOutput>> GetPaginatedDataAsync(int limit, int offset);
+    public Task AddAsync(TInput dto);
+    public Task EditAsync(TInput dto);
+    public Task DeleteAsync(int id);
+    public Task<TOutput> GetForViewByIdAsync(int id);
+    public Task<TOutput> GetForEditByIdAsync(int id);
+    public Task<TOutput> GetForDeleteByIdAsync(int id);
+    public Task<List<TOutput>> GetPaginatedDataAsync(int limit, int offset);
 }
