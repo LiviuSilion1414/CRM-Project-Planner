3960f93b703a4b3762969dd0383ef3e886612c35
liviusilion-dev <silionliviu001@gmail.com>
2024-06-28 11:23:38 +0200

    Apply mapped queries

--- PlannerCRM/Server/Repositories/EmployeeRepository.cs
+++ PlannerCRM/Server/Repositories/EmployeeRepository.cs
@@ -30,38 +30,7 @@ public class EmployeeRepository
             var isValid = await _validator.ValidateEmployeeAsync(dto, OperationType.ADD);
             
             if (isValid) {
-                await _dbContext.Employees.AddAsync(
-                    new Employee
-                    {
-                        Email = dto.Email,
-                        FirstName = dto.FirstName,
-                        LastName = dto.LastName,
-                        FullName = $"{dto.FirstName} {dto.LastName}",
-                        Username = dto.Email,
-                        BirthDay = dto.BirthDay
-                            ?? throw new NullReferenceException(ExceptionsMessages.NULL_ARG),
-                        StartDate = dto.StartDate
-                            ?? throw new NullReferenceException(ExceptionsMessages.NULL_ARG),
-                        Password = dto.Password,
-                        NumericCode = dto.NumericCode,
-                        Role = dto.Role
-                            ?? throw new NullReferenceException(ExceptionsMessages.NULL_ARG),
-                        CurrentHourlyRate = dto.CurrentHourlyRate,
-                        Salaries = dto.EmployeeSalaries
-                            .Select(ems =>
-                                new EmployeeSalary
-                                {
-                                    StartDate = ems.StartDate,
-                                    FinishDate = ems.FinishDate,
-                                    Salary = ems.Salary
-                                }
-                            )
-                            .ToList()
-                    }
-                );
-
-                
-                //await _userManager.AddToRoleAsync(user, dto.Role.ToString() ?? throw new ArgumentNullException(nameof(dto.Role), ExceptionsMessages.NULL_ARG));
+                await _dbContext.Employees.AddAsync(dto.MapToEmployee());
                 
                 if (await _dbContext.SaveChangesAsync() == 0) {
                     throw new DbUpdateException(ExceptionsMessages.IMPOSSIBLE_SAVE_CHANGES);
@@ -124,13 +93,7 @@ public class EmployeeRepository
                 .SingleAsync(em => em.Email == dto.Email);
 
             await _dbContext.ProfilePictures.AddAsync(
-                new EmployeeProfilePicture
-                {
-                    ImageType = dto.ProfilePicture.ImageType,
-                    Thumbnail = dto.ProfilePicture.Thumbnail,
-                    EmployeeId = employee.Id,
-                    EmployeeInfo = employee
-                }
+                dto.MapToEmployeeProfilePicture(employee)
             );
 
             await _dbContext.SaveChangesAsync();
@@ -246,20 +209,8 @@ public class EmployeeRepository
                 var user = await _userManager.FindByEmailAsync(dto.OldEmail);
                 var model = await _dbContext.Employees.SingleAsync(em => em.Id == dto.Id);
 
-                model.FirstName = dto.FirstName;
-                model.LastName = dto.LastName;
-                model.FullName = $"{dto.FirstName + dto.LastName + string.Empty}";
-                model.Username = dto.Email;
-                model.BirthDay = dto.BirthDay 
-                    ?? throw new NullReferenceException(ExceptionsMessages.NULL_ARG);
-                model.StartDate = dto.StartDate 
-                    ?? throw new NullReferenceException(ExceptionsMessages.NULL_ARG);
-                model.Email = dto.Email;
-                model.Role = dto.Role 
-                    ?? throw new NullReferenceException(ExceptionsMessages.NULL_ARG);
-                model.NumericCode = dto.NumericCode;
-                model.CurrentHourlyRate = dto.CurrentHourlyRate;
-                
+                model = dto.MapToEmployee();
+
                 var isContainedModifiedHourlyRate = await _dbContext.Employees
                     .AnyAsync(em => em.Salaries
                         .Any(s => s.Salary != dto.CurrentHourlyRate));
@@ -268,14 +219,8 @@ public class EmployeeRepository
                     model.Salaries = dto.EmployeeSalaries
                         .Where(ems => _dbContext.Employees
                             .Any(em => em.Id == ems.EmployeeId))
-                        .Select(ems => 
-                            new EmployeeSalary {
-                                EmployeeId = dto.Id,
-                                StartDate = ems.StartDate,
-                                FinishDate = ems.FinishDate,
-                                Salary = ems.Salary
-                            }
-                        ).ToList();
+                        .Select(ems => ems.MapToEmployeeSalary(dto))
+                        .ToList();
                 }    
                 
                 _dbContext.Employees.Update(model);
@@ -326,171 +271,36 @@ public class EmployeeRepository
 
     public async Task<EmployeeViewDto> GetForViewByIdAsync(string employeeId) {
         return await _dbContext.Employees
-            .Select(em => new EmployeeViewDto {
-                Id = em.Id,
-                FirstName = em.FirstName,
-                LastName = em.LastName,
-                FullName = $"{em.FirstName} {em.LastName}",
-                BirthDay = em.BirthDay,
-                StartDate = em.StartDate,
-                NumericCode = em.NumericCode,
-                Password = em.Password,
-                StartDateHourlyRate = em.Salaries.Single().StartDate,
-                FinishDateHourlyRate = em.Salaries.Single().FinishDate,
-                Email = em.Email,
-                IsDeleted = em.IsDeleted,
-                IsArchived = em.IsArchived,
-                Role = em.Role, 
-                CurrentHourlyRate = em.CurrentHourlyRate,
-                ProfilePicture = new ProfilePictureDto {
-                    Id = em.ProfilePictureId,
-                    EmployeeId = em.Id,
-                    EmployeeInfo = new EmployeeSelectDto {
-                        Email = em.Email,
-                        FirstName = em.FirstName,
-                        LastName = em.LastName,
-                        FullName = $"{em.FirstName} {em.LastName}",
-                        CurrentHourlyRate = em.CurrentHourlyRate,
-                        Role = em.Role
-                    },
-                    Thumbnail = _dbContext.ProfilePictures
-                        .Single(pp => pp.EmployeeId == em.Id)
-                        .Thumbnail,
-                    ImageType = _dbContext.ProfilePictures
-                        .Single(pp => pp.EmployeeId == em.Id)
-                        .ImageType
-                },
-                EmployeeSalaries = em.Salaries
-                    .Select(ems => new EmployeeSalaryDto {
-                        Id = ems.Id,
-                        EmployeeId = ems.Id,
-                        StartDate = ems.StartDate,
-                        FinishDate = ems.StartDate,
-                        Salary = ems.Salary})
-                    .ToList(),
-                EmployeeActivities = em.EmployeeActivity
-                    .Select(ea =>
-                        new EmployeeActivityDto {
-                            EmployeeId = ea.EmployeeId,
-                            Employee = new EmployeeSelectDto {
-                                Id = ea.Employee.Id,
-                                Email = ea.Employee.Email,
-                                FirstName = ea.Employee.FirstName,
-                                LastName = ea.Employee.LastName,
-                                Role = ea.Employee.Role,
-                                CurrentHourlyRate = ea.Employee.CurrentHourlyRate,
-                            },
-                            ActivityId = ea.ActivityId,
-                            Activity = new ActivitySelectDto {
-                                Id = ea.Activity.Id,
-                                Name = ea.Activity.Name,
-                                StartDate = ea.Activity.StartDate,
-                                FinishDate = ea.Activity.FinishDate,
-                                WorkOrderId = ea.Activity.WorkOrderId
-                            }
-                        })
-                    .ToList()
-                })   
+            .Select(em => em.MapToEmployeeViewDto(_dbContext))   
             .SingleAsync(em => em.Id == employeeId);
     }
 
     public async Task<EmployeeSelectDto> GetForRestoreAsync(string employeeId) {
         return await _dbContext.Employees
-            .Select(em => new EmployeeSelectDto {
-                Id = em.Id,
-                Email = em.Email,
-                FirstName = em.FirstName,
-                LastName = em.LastName,
-                FullName = em.FullName,
-                Role = em.Role
-            })
+            .Select(em => em.MapToEmployeeSelectDto())
             .SingleAsync(em => em.Id == employeeId);
     }
 
     public async Task<EmployeeFormDto> GetForEditByIdAsync(string employeeId) {
         return await _dbContext.Employees
             .Where(em => !em.IsDeleted || !em.IsArchived)
-            .Select(em => new EmployeeFormDto {
-                Id = em.Id,
-                FirstName = em.FirstName,
-                LastName = em.LastName,
-                Email = em.Email,
-                OldEmail = em.Email,
-                BirthDay = em.BirthDay,
-                StartDate = em.StartDate,
-                Role = em.Role,
-                NumericCode = em.NumericCode,
-                Password = em.Password,
-                CurrentHourlyRate = em.CurrentHourlyRate,
-                IsDeleted = em.IsDeleted,
-                StartDateHourlyRate = em.Salaries.Single().StartDate,
-                FinishDateHourlyRate = em.Salaries.Single().FinishDate,
-                EmployeeSalaries = em.Salaries
-                    .Where(ems => _dbContext.Employees
-                        .Any(em => em.Id == ems.EmployeeId))
-                    .Select(ems => new EmployeeSalaryDto {
-                        Id = ems.Id,
-                        EmployeeId = em.Id,
-                        StartDate = ems.StartDate,
-                        FinishDate = ems.StartDate,
-                        Salary = ems.Salary})
-                    .ToList()
-                })
+            .Select(em => em.MapToEmployeeFormDto(_dbContext))
             .SingleAsync(em => em.Id == employeeId);
     }
 
     public async Task<EmployeeDeleteDto> GetForDeleteByIdAsync(string employeeId) {
         return await _dbContext.Employees
             .Where(em => (!em.IsDeleted || !em.IsArchived) && em.Id == employeeId)
-            .Select(em => new EmployeeDeleteDto {
-                Id = em.Id,
-                FullName = $"{em.FirstName} {em.LastName}",
-                Email = em.Email,
-                Role = em.Role
-                    .ToString()
-                    .Replace('_', ' '),
-                EmployeeActivities = _dbContext.EmployeeActivity
-                    .Where(ea => ea.EmployeeId == employeeId)
-                    .Select(ea => new EmployeeActivityDto {
-                        Id = ea.Id,
-                        EmployeeId = ea.EmployeeId,
-                        Employee = _dbContext.Employees
-                            .Where(e => e.Id == ea.EmployeeId)
-                            .Select(_ => new EmployeeSelectDto {
-                                Id = ea.Employee.Id,
-                                Email = ea.Employee.Email,
-                                FullName = ea.Employee.FullName,
-                            })
-                            .Single(),
-                        ActivityId = ea.ActivityId,
-                        Activity = _dbContext.Activities
-                            .Where(ac => ac.Id == ea.ActivityId)
-                            .Select(_ => new ActivitySelectDto {
-                                Id = ea.Activity.Id,
-                                Name = ea.Activity.Name,
-                                WorkOrderId = ea.Activity.WorkOrderId,
-                            })
-                            .Single()
-                    })
-                    .ToList()
-                
-            })
+            .Select(em => em.MapToEmployeeDeleteDto(employeeId, _dbContext))
             .SingleAsync(em => em.Id == employeeId);
     }
     
     public async Task<List<EmployeeSelectDto>> SearchEmployeeAsync(string email) {
         return await _dbContext.Employees
             .Where(em => !em.IsDeleted && !em.IsArchived)
-            .Select(em => new EmployeeSelectDto {
-                Id = em.Id,
-                Email = em.Email,
-                FirstName = em.FirstName,
-                LastName = em.LastName,
-                FullName = em.FullName,
-                Role = em.Role
-            })
             .Where(em => EF.Functions.ILike(em.FullName, $"%{email}%") || 
                 EF.Functions.ILike(em.Email, $"%{email}%"))
+            .Select(em => em.MapToEmployeeSelectDto())
             .ToListAsync();
     }
 
@@ -499,64 +309,17 @@ public class EmployeeRepository
             .OrderBy(em => em.Id)
             .Skip(limit)
             .Take(offset)
-            .Select(em => new EmployeeViewDto {
-                Id = em.Id,
-                FirstName = em.FirstName,
-                LastName = em.LastName,
-                FullName = $"{em.FirstName} {em.LastName}",
-                BirthDay = em.BirthDay,
-                StartDate = em.StartDate,
-                Email = em.Email,
-                Role = em.Role,
-                CurrentHourlyRate = em.CurrentHourlyRate,
-                IsDeleted = em.IsDeleted,
-                IsArchived = em.IsArchived,
-                EmployeeActivities = em.EmployeeActivity
-                    .Select(ea => new EmployeeActivityDto {
-                        Id = ea.ActivityId,
-                        EmployeeId = em.Id,
-                        Employee = _dbContext.Employees
-                            .Select(em => new EmployeeSelectDto {
-                                Id = em.Id,
-                                Email = em.Email,
-                                FirstName = em.FirstName,
-                                LastName = em.LastName,
-                                Role = em.Role
-                            })
-                            .Single(em => em.Id == ea.EmployeeId),
-                        ActivityId = ea.ActivityId,
-                        Activity = _dbContext.Activities
-                            .Select(ac => new ActivitySelectDto {
-                            Id = ac.Id,
-                            Name = ac.Name,
-                            StartDate = ac.StartDate,
-                            FinishDate = ac.FinishDate,
-                            WorkOrderId = ac.WorkOrderId
-                        })
-                        .Single(ac => ac.Id == ea.ActivityId),
-                    }).ToList(),
-                EmployeeSalaries = em.Salaries
-                    .Select(ems => new EmployeeSalaryDto {
-                        Id = ems.Id,
-                        EmployeeId = ems.Id,
-                        StartDate = ems.StartDate,
-                        FinishDate = ems.StartDate,
-                        Salary = ems.Salary})
-                    .ToList(),
-            })
+            .Select(em => em.MapToEmployeeViewDto(_dbContext))
             .ToListAsync();
     }
 
     public async Task<CurrentEmployeeDto> GetEmployeeIdAsync(string email) {
         return await _dbContext.Employees
             .Where(em => !em.IsDeleted && !em.IsArchived)
-            .Select(em => new CurrentEmployeeDto {
-                Id = em.Id,
-                Email = em.Email})
+            .Select(em => em.MapToCurrentEmployeeDto())
             .FirstAsync(em => em.Email == email);
     }
 
     public async Task<int> GetEmployeesSizeAsync() => 
-        await _dbContext.Employees
-            .CountAsync();
+        await _dbContext.Employees.CountAsync();
 }
