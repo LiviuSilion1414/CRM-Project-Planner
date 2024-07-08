3de78a6e308c85e369138d1b5a8162ea04ee4732
liviusilion-dev <silionliviu001@gmail.com>
2024-07-08 17:51:40 +0200

    Apply migrations

---
+++ PlannerCRM/Server/Migrations/20240708154957_UpdateEntityProps.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlannerCRM.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEntityProps : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                schema: "public",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                schema: "public",
+                table: "AspNetUsers",
+                newName: "UserName");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                schema: "public",
+                table: "AspNetUsers",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserName",
+                schema: "public",
+                table: "AspNetUsers",
+                newName: "Username");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                schema: "public",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(256)",
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                schema: "public",
+                table: "AspNetUsers",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: true);
+        }
+    }
+}
