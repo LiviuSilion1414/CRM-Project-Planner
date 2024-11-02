b5b34fc322bc9cca3d3f1235acc6277523384826
liviusilion-dev <silionliviu001@gmail.com>
2024-11-02 18:29:11 +0100

    Implement generic CRUD repo

--- PlannerCRM/Server/Interfaces/IRepository.cs
+++ PlannerCRM/Server/Interfaces/IRepository.cs
@@ -3,7 +3,9 @@ namespace PlannerCRM.Server.Interfaces;
 public interface IRepository<TInput>
     where TInput : class
 {
-    public Task AddAsync(TInput dto);
-    public Task EditAsync(TInput dto);
+    public Task AddAsync(TInput model);
+    public Task EditAsync(TInput model, int id);
     public Task DeleteAsync(int id);
+    public Task<TInput> GetByIdAsync(int id);
+    public Task<ICollection<TInput>> GetWithPagination(int offset, int limit);
 }
---
+++ PlannerCRM/Server/Repositories/Repository.cs
@@ -0,0 +1,48 @@
+﻿namespace PlannerCRM.Server.Repositories;
+
+public class Repository<TInput>(AppDbContext context) : IRepository<TInput>
+    where TInput : class
+{
+    private readonly AppDbContext _context = context;
+
+    public async Task AddAsync(TInput model)
+    {
+        await _context.Set<TInput>().AddAsync(model);
+
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var model = await _context.Set<TInput>().FindAsync(id);
+        
+        _context.Set<TInput>().Remove(model);
+
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task EditAsync(TInput model, int id)
+    {
+        var item = await _context.Set<TInput>().FindAsync(id);
+        
+        item = model;
+
+        _context.Set<TInput>().Update(item);
+
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<TInput> GetByIdAsync(int id)
+    {
+        return await _context.Set<TInput>().FindAsync(id);
+    }
+
+    public async Task<ICollection<TInput>> GetWithPagination(int offset, int limit)
+    {
+        return await _context
+            .Set<TInput>()
+            .Skip(offset)
+            .Take(limit)
+            .ToListAsync();
+    }
+}
