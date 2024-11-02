62380110367cdd64475abc67ee3e02a45d3ca98a
liviusilion-dev <silionliviu001@gmail.com>
2024-11-02 18:37:41 +0100

    Use builder pattern

---
+++ PlannerCRM/Server/Extensions/PipelineBuilderExtension.cs
@@ -0,0 +1,64 @@
+﻿using PlannerCRM.Server.Models.Entities;
+using PlannerCRM.Server.Models.JunctionEntities;
+
+namespace PlannerCRM.Server.Extensions;
+
+public static class PipelineBuilderExtension
+{
+    public static void ConfigureDbConnectionString(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseNpgsql(
+                builder.Configuration
+                    .GetConnectionString("ConnString")
+                        ?? throw new InvalidOperationException("ConnString not found!"))
+);
+    }
+
+    public static void ConfigureIdentityOptions(this IServiceCollection services)
+    {
+        services
+            .AddIdentity<Employee, EmployeeRole>()
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddUserManager<UserManager<Employee>>()
+            .AddDefaultTokenProviders();
+
+        services.Configure<IdentityOptions>(o =>
+        {
+            o.User.RequireUniqueEmail = true;
+            o.SignIn.RequireConfirmedEmail = false;
+            o.Password.RequireNonAlphanumeric = false;
+            o.Password.RequireDigit = false;
+            o.Password.RequiredLength = 8;
+            o.Lockout.AllowedForNewUsers = true;
+            o.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            o.Lockout.MaxFailedAccessAttempts = 5;
+        });
+    }
+
+    public static void ConfigureCookiePolicy(this IServiceCollection services)
+    {
+        services.Configure<CookiePolicyOptions>(options =>
+        {
+            options.CheckConsentNeeded = context => true;
+            options.MinimumSameSitePolicy = SameSiteMode.None;
+        });
+
+    }
+
+    public static void RegisterServices(this IServiceCollection services)
+    {
+        services.AddScoped<CalculatorService>();
+        services.AddScoped<DtoValidatorUtillity>();
+        services.AddScoped<IRepository<EmployeeFormDto>, EmployeeRepository>();
+        services.AddScoped<IRepository<WorkOrderFormDto>, WorkOrderRepository>();
+        services.AddScoped<IRepository<ActivityFormDto>, ActivityRepository>();
+        services.AddScoped<IRepository<WorkTimeRecordFormDto>, WorkTimeRecordRepository>();
+        services.AddScoped<IRepository<ClientFormDto>, ClientRepository>();
+        services.AddScoped<IActivityRepository, ActivityRepository>();
+        services.AddScoped<IClientRepository, ClientRepository>();
+        services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+        services.AddScoped<IWorkOrderRepository, WorkOrderRepository>();
+        services.AddScoped<IWorkTimeRecordRepository, WorkTimeRecordRepository>();
+    }
+}
--- PlannerCRM/Server/Program.cs
+++ PlannerCRM/Server/Program.cs
@@ -1,58 +1,21 @@
 using PlannerCRM.Server.Extensions;
 using PlannerCRM.Server.Models.Entities;
+using PlannerCRM.Server.Models.JunctionEntities;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(
-        builder.Configuration
-            .GetConnectionString("ConnString")
-                ?? throw new InvalidOperationException("ConnString not found!"))
-);
+builder.ConfigureDbConnectionString();
 
 builder.Services.AddHttpClient();
 
-builder.Services
-    .AddIdentity<Employee, EmployeeRole>()
-    .AddEntityFrameworkStores<AppDbContext>()
-    .AddUserManager<UserManager<Employee>>()
-    .AddDefaultTokenProviders();
+builder.Services.ConfigureIdentityOptions();
 
-builder.Services.Configure<CookiePolicyOptions>(options =>
-{
-    options.CheckConsentNeeded = context => true;
-    options.MinimumSameSitePolicy = SameSiteMode.None;
-});
+builder.Services.ConfigureCookiePolicy();
 
-builder.Services
-    .Configure<IdentityOptions>(o =>
-    {
-        o.User.RequireUniqueEmail = true;
-        o.SignIn.RequireConfirmedEmail = false;
-        o.Password.RequireNonAlphanumeric = false;
-        o.Password.RequireDigit = false;
-        o.Password.RequiredLength = 8;
-        o.Lockout.AllowedForNewUsers = true;
-        o.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-        o.Lockout.MaxFailedAccessAttempts = 5;
-    });
-builder.Services.AddScoped<CalculatorService>();
-builder.Services.AddScoped<DtoValidatorUtillity>();
-
-builder.Services.AddScoped<IRepository<EmployeeFormDto>, EmployeeRepository>();
-builder.Services.AddScoped<IRepository<WorkOrderFormDto>, WorkOrderRepository>();
-builder.Services.AddScoped<IRepository<ActivityFormDto>, ActivityRepository>();
-builder.Services.AddScoped<IRepository<WorkTimeRecordFormDto>, WorkTimeRecordRepository>();
-builder.Services.AddScoped<IRepository<ClientFormDto>, ClientRepository>();
-
-builder.Services.AddScoped<IActivityRepository, ActivityRepository>();
-builder.Services.AddScoped<IClientRepository, ClientRepository>();
-builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
-builder.Services.AddScoped<IWorkOrderRepository, WorkOrderRepository>();
-builder.Services.AddScoped<IWorkTimeRecordRepository, WorkTimeRecordRepository>();
+builder.Services.RegisterServices();
 
 builder.Logging.AddConfiguration(
     builder.Configuration.GetSection("Logging"));
@@ -60,10 +23,6 @@ var app = builder.Build();
 
 using var scope = app.Services.CreateScope();
 var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-
-if (!await context.Users.AnyAsync()) {
-    await app.SeedDataAsync();
-}
 
 if (app.Environment.IsDevelopment()) {
     app.UseWebAssemblyDebugging();
