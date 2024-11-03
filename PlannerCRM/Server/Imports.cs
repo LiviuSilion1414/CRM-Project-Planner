d2c9e94df7b9a316da2a0a410b957eff5995c6cd
liviusilion-dev <silionliviu001@gmail.com>
2024-11-03 18:13:38 +0100

    Update global imports file

--- PlannerCRM/Server/Imports.cs
+++ PlannerCRM/Server/Imports.cs
@@ -5,6 +5,7 @@ global using Microsoft.AspNetCore.Identity;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.AspNetCore.Diagnostics;
 global using Microsoft.AspNetCore.Mvc.RazorPages;
+global using AutoMapper;
 
 global using PlannerCRM.Shared.Attributes;
 global using PlannerCRM.Shared.Constants;
@@ -15,7 +16,6 @@ global using PlannerCRM.Shared.Feedbacks;
 global using PlannerCRM.Server.DataAccess;
 global using PlannerCRM.Server.Extensions;
 global using PlannerCRM.Server.Interfaces;
-global using PlannerCRM.Server.Mappers;
 global using PlannerCRM.Server.Models;
 global using PlannerCRM.Server.Repositories;
 global using PlannerCRM.Server.Services;
