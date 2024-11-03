9ce003264674fc0e775b1bfa76187831d6d2f694
liviusilion-dev <silionliviu001@gmail.com>
2024-11-03 15:47:36 +0100

    Update global imports file

---
+++ PlannerCRM/Shared/Imports.cs
@@ -0,0 +1,8 @@
+global using System.ComponentModel.DataAnnotations;
+global using System.Text.RegularExpressions;
+
+global using PlannerCRM.Shared.Constants;
+global using PlannerCRM.Shared.Models;
+global using PlannerCRM.Shared.Dtos.Entities;
+global using PlannerCRM.Shared.Dtos.JunctionEntities;
+global using PlannerCRM.Shared.Feedbacks;
