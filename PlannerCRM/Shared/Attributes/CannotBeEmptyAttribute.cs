91f63f496819513117ec3d8dd557bca1c31b9481
liviusilion-dev <silionliviu001@gmail.com>
2024-11-03 15:44:13 +0100

    Remove major custom validation attributes

--- PlannerCRM/Shared/Attributes/CannotBeEmptyAttribute.cs
+++
@@ -1,8 +0,0 @@
-namespace PlannerCRM.Shared.Attributes;
-
-public class CannotBeEmptyAttribute : ValidationAttribute
-{
-    public override bool IsValid(object value) =>
-        value is not null && (value as IEnumerable<object>)
-            .Any();
-}
--- PlannerCRM/Shared/Attributes/StringIsNotNullOrEmptyAttribute.cs
+++
@@ -1,8 +0,0 @@
-namespace PlannerCRM.Shared.Attributes;
-
-public class StringIsNotNullOrEmptyAttribute : ValidationAttribute
-{
-    public override bool IsValid(object value) {
-        return !string.IsNullOrEmpty(value as string);
-    }
-}
