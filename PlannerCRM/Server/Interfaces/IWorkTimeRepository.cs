e5b4391d78f2679ca83c3c4b4541d51dee8c1a1c
liviusilion-dev <silionliviu001@gmail.com>
2024-11-03 18:13:16 +0100

    Remove old repos

--- PlannerCRM/Server/Interfaces/IWorkTimeRepository.cs
+++
@@ -1,8 +0,0 @@
-namespace PlannerCRM.Server.Interfaces;
-
-public interface IWorkTimeRepository
-{
-    public Task<List<WorkTimeRecordViewDto>> GetPaginatedWorkTimeRecordsAsync(int limit, int offset);
-    public Task<WorkTimeRecordViewDto> GetAllWorkTimeRecordsByEmployeeIdAsync(int employeeId);
-    public Task<WorkTimeRecordViewDto> GetForViewByIdAsync(int workOrderId, int activityId, int employeeId);
-}
