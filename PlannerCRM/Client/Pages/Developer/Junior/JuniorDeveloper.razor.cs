33c153639a25b2389b92613dc628be565f62757c
liviusilion-dev <silionliviu001@gmail.com>
2024-11-05 12:30:13 +0100

    Remove old&tightly-coupled components

--- PlannerCRM/Client/Pages/Developer/Junior/JuniorDeveloper.razor.cs
+++
@@ -1,7 +0,0 @@
-namespace PlannerCRM.Client.Pages.Developer.Junior;
-
-[Authorize(Roles = nameof(Roles.JUNIOR_DEVELOPER))]
-public partial class JuniorDeveloper : ComponentBase
-{
-    [Parameter] public int EmployeeId { get; set; }
-}
