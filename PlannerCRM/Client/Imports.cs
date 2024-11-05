088fbfdd60249737fc2cdc6f94abbcccb68dd5bd
liviusilion-dev <silionliviu001@gmail.com>
2024-11-05 12:20:32 +0100

    Update Imports file

---
+++ PlannerCRM/Client/Imports.cs
@@ -0,0 +1,19 @@
+global using Microsoft.AspNetCore.Components.Authorization;
+global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+global using Microsoft.AspNetCore.Components;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Components.Forms;
+global using System.ComponentModel.DataAnnotations;
+global using System.Net.Http.Json;
+global using System.Security.Claims;
+global using System.Reflection;
+
+global using PlannerCRM.Client.Utilities.Navigation;
+global using PlannerCRM.Client.Utilities.Validation;
+global using PlannerCRM.Client.Services.Crud;
+
+global using PlannerCRM.Shared.Dtos.Entities;
+global using PlannerCRM.Shared.Dtos.JunctionEntities;
+global using PlannerCRM.Shared.Feedbacks;
+global using PlannerCRM.Shared.Constants;
