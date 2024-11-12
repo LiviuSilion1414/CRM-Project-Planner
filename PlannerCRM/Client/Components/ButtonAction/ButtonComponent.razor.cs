2d22059eb77f08954781215bca4a42b7f4da4260
liviusilion-dev <silionliviu001@gmail.com>
2024-11-12 14:08:31 +0100

    Remove old components

--- PlannerCRM/Client/Components/ButtonAction/ButtonComponent.razor.cs
+++
@@ -1,9 +0,0 @@
-namespace PlannerCRM.Client.Components.ButtonAction;
-
-public partial class ButtonComponent : ComponentBase
-{
-    [Parameter] public string Type { get; set; }
-    [Parameter] public string Class { get; set; }
-    [Parameter] public string InnerText { get; set; }
-    [Parameter] public Action RunMethod { get; set; }
-}
