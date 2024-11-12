7317773939e23d30d4dc48e4d9d2557d525654fd
liviusilion-dev <silionliviu001@gmail.com>
2024-11-12 15:38:30 +0100

    Update imports

--- PlannerCRM/Client/Imports.cs
+++ PlannerCRM/Client/Imports.cs
@@ -1,6 +1,8 @@
 global using Microsoft.AspNetCore.Components.Web;
 global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+global using Radzen;
+
 global using PlannerCRM.Client;
 
 global using PlannerCRM.Shared.Constants;
