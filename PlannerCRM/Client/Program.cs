17d7ccdd6be03e5f889eceb93b7bba5c1e0ab539
liviusilion-dev <silionliviu001@gmail.com>
2024-11-12 14:25:41 +0100

    Remove old services

--- PlannerCRM/Client/Program.cs
+++ PlannerCRM/Client/Program.cs
@@ -1,6 +1,3 @@
-using PlannerCRM.Client;
-using PlannerCRM.Client.Utilities.Navigation;
-
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.RootComponents.Add<App>("#app");
@@ -8,34 +5,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddOptions();
 builder.Services.AddAuthorizationCore();
-
-builder.Services.AddScoped<AuthenticationStateProvider>(s => 
-    s.GetRequiredService<AuthenticationStateService>()
-);
-
-builder.Services.AddScoped<LoginService>();
-builder.Services.AddScoped<AuthenticationStateService>();
-builder.Services.AddScoped<CurrentUserInfoService>();
-builder.Services.AddScoped<NavigationLockService>();
-
-builder.Services.AddScoped<ProjectManagerService>();
-builder.Services.AddScoped<OperationManagerCrudService>();
-builder.Services.AddScoped<AccountManagerCrudService>();
-builder.Services.AddScoped<DeveloperService>();
-builder.Services.AddScoped<CustomDataAnnotationsValidator>();
-
-builder.Services.AddScoped<Logger<AuthenticationStateService>>();
-builder.Services.AddScoped<Logger<CurrentUserInfoService>>();
-builder.Services.AddScoped<Logger<AccountManagerCrudService>>();
-builder.Services.AddScoped<Logger<OperationManagerCrudService>>();
-builder.Services.AddScoped<Logger<ProjectManagerService>>();
-
-builder.Services.AddScoped<Logger<LoginService>>();
-builder.Services.AddScoped<Logger<DeveloperService>>();
-builder.Services.AddScoped<Logger<EmployeeLoginDto>>();
-builder.Services.AddScoped<Logger<EmployeeFormDto>>();
-builder.Services.AddScoped<Logger<WorkOrderFormDto>>();
-builder.Services.AddScoped<Logger<ActivityFormDto>>();
 
 builder.Services.AddScoped(_ => 
     new HttpClient {
