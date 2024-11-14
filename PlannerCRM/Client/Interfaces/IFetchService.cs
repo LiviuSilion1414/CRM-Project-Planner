0ef0747708444751cbe286360ac95451a4988835
liviusilion-dev <silionliviu001@gmail.com>
2024-11-14 22:23:55 +0100

    Define fetching interface

---
+++ PlannerCRM/Client/Interfaces/IFetchService.cs
@@ -0,0 +1,11 @@
+﻿namespace PlannerCRM.Client.Interfaces;
+
+public interface IFetchService<TItem>
+    where TItem : class
+{
+    Task Create(TItem item);
+    Task<TItem> Read(int itemId);
+    Task Update(TItem item);
+    Task Delete(int itemId);
+    Task<ICollection<TItem>> GetAll();
+}
