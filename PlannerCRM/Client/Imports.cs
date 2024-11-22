556ce723aaf5b8f85f7aeaf035f4126ccf081ec1
liviusilion-dev <silionliviu001@gmail.com>
2024-11-22 13:33:21 +0100

    Fix deselection in datagrid

--- PlannerCRM/Client/Imports.cs
+++ PlannerCRM/Client/Imports.cs
@@ -1,3 +1,4 @@
+global using Microsoft.AspNetCore.Components.Forms;
 global using Microsoft.AspNetCore.Components;
 global using Microsoft.AspNetCore.Components.Web;
 global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
