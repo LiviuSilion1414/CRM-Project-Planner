1b0852d6faf754361829d2f7f2385aa7040f7497
liviusilion-dev <silionliviu001@gmail.com>
2024-12-01 18:30:15 +0100

    Delete old components

--- PlannerCRM/Client/GlobalImports.cs
+++ PlannerCRM/Client/GlobalImports.cs
@@ -1,19 +1,3 @@
 global using Microsoft.AspNetCore.Components;
-global using Microsoft.AspNetCore.Authorization;
-global using Microsoft.AspNetCore.Components.Forms;
-global using System.Net.Http.Json;
-global using Microsoft.AspNetCore.Components.Authorization;
 global using Microsoft.AspNetCore.Components.Web;
 global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-global using System.Security.Claims;
-
-global using PlannerCRM.Client.Services.Crud;
-global using PlannerCRM.Client.Utilities.Navigation;
-global using PlannerCRM.Client.Utilities.Validation;
-global using System.ComponentModel.DataAnnotations;
-global using System.Reflection;
-global using Newtonsoft.Json;
-
-global using PlannerCRM.Shared.Feedbacks;
-global using PlannerCRM.Shared.Constants;
-global using PlannerCRM.Client.Components.ValidationAndNavigation.ValidatorComponent.CustomValidatorComponent;
--- PlannerCRM/Client/Pages/Developer/Senior/SeniorDeveloper.razor.cs
+++
@@ -1,7 +0,0 @@
-namespace PlannerCRM.Client.Pages.Developer.Senior;
-
-[Authorize(Roles = nameof(Roles.SENIOR_DEVELOPER))]
-public partial class SeniorDeveloper : ComponentBase
-{
-    [Parameter] public int EmployeeId { get; set; }
-}
