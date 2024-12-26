b1156561349b80e98f9fcbffe8b12fd8132cf7ba
liviusilion-dev <silionliviu001@gmail.com>
2024-12-26 21:33:54 +0100

    working draft: add generic datagrid

--- PlannerCRM/Client/Imports.cs
+++ PlannerCRM/Client/Imports.cs
@@ -2,6 +2,7 @@ global using Microsoft.AspNetCore.Components.Forms;
 global using Microsoft.AspNetCore.Components;
 global using Microsoft.AspNetCore.Components.Web;
 global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+global using System.Reflection;
 
 global using Radzen;
 global using Radzen.Blazor;
