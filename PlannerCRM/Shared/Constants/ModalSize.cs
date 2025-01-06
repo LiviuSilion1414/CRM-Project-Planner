b9bc9388e7284faa7dd0189d64cd8897ed70c98d
liviusilion-dev <silionliviu001@gmail.com>
2025-01-06 22:20:26 +0100

    Update modal size values

--- PlannerCRM/Shared/Constants/ModalSize.cs
+++ PlannerCRM/Shared/Constants/ModalSize.cs
@@ -2,9 +2,9 @@ namespace PlannerCRM.Shared.Constants;
 
 public class ModalSize
 {
-    public const string FULL_SCREEN = "modal-dialog modal-fullscreen";
-    public const string EXTRA_LARGE = "modal-dialog modal-xl";
-    public const string LARGE = "modal-dialog modal-lg";
-    public const string DEFAULT = "modal-dialog";
-    public const string SMALL = "modal-dialog modal-sm";
+    public const string FULL_SCREEN = "modal-fullscreen";
+    public const string EXTRA_LARGE = "modal-xl";
+    public const string LARGE = "modal-lg";
+    public const string DEFAULT = "";
+    public const string SMALL = "modal-sm";
 }
