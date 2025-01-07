5305ca41cb6a6015fc8366317b89739796dd1c59
liviusilion-dev <silionliviu001@gmail.com>
2025-01-07 15:32:07 +0100

    Improve dto validation

--- PlannerCRM/Shared/Dtos/Entities/ActivityDto.cs
+++ PlannerCRM/Shared/Dtos/Entities/ActivityDto.cs
@@ -1,17 +1,30 @@
-﻿using PlannerCRM.Shared.Dtos.JunctionEntities;
-
-namespace PlannerCRM.Shared.Dtos.Entities;
+﻿namespace PlannerCRM.Shared.Dtos.Entities;
 
 public class ActivityDto
 {
     public int Id { get; set; }
+
+    [Required]
+    [MinLength(5)]
     public string Name { get; set; }
+
+    [Required]
+    [PresentOrFutureDate]
     public DateTime CreationDate { get; set; }
+
+    [Required]
+    [PresentOrFutureDate]
     public DateTime StartDate { get; set; }
+
+    [Required]
+    [PresentOrFutureDate]
     public DateTime EndDate { get; set; }
+
+    [Required]
     public int WorkOrderId { get; set; }
 
     // Navigation properties
+    [Required]
     public WorkOrderDto WorkOrderDto { get; set; }
     public ICollection<EmployeeDto> EmployeesDto { get; set; }
     public ICollection<EmployeeActivityDto> EmployeeActivitiesDto { get; set; }
