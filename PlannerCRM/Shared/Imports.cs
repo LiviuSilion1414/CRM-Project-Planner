782d1e77f4aa269531689a8087052ccc90bcf904
liviusilion-dev <silionliviu001@gmail.com>
2025-01-07 15:22:54 +0100

    Refactor code

--- PlannerCRM/Shared/Imports.cs
+++ PlannerCRM/Shared/Imports.cs
@@ -4,4 +4,5 @@ global using System.Text.RegularExpressions;
 global using PlannerCRM.Shared.Constants;
 global using PlannerCRM.Shared.Dtos.Entities;
 global using PlannerCRM.Shared.Dtos.JunctionEntities;
+global using PlannerCRM.Shared.Attributes;
 global using PlannerCRM.Shared.Feedbacks;
