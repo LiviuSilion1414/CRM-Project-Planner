f5834c804a0be2ad9c48528f6c50b0ed2f40f233
liviusilion-dev <silionliviu001@gmail.com>
2025-01-07 15:20:25 +0100

    Add custom validation attribute

---
+++ PlannerCRM/Shared/Attributes/PresentOrFutureDateAttribute.cs
@@ -0,0 +1,16 @@
+﻿namespace PlannerCRM.Shared.Attributes;
+
+public sealed class PresentOrFutureDateAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        // your validation logic
+        if (Convert.ToDateTime(value) >= DateTime.Today)
+        {
+            return ValidationResult.Success;
+        } else
+        {
+            return new ValidationResult("Past date not allowed.");
+        }
+    }
+}
