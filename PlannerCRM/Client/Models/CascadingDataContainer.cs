7016c632df17b2cda9061aca0c325ffd90e7c354
liviusilion-dev <silionliviu001@gmail.com>
2025-01-09 00:05:04 +0100

    Improve how data is passed through components using a data container

--- PlannerCRM/Client/Models/CascadingDataContainer.cs
+++ PlannerCRM/Client/Models/CascadingDataContainer.cs
@@ -1,12 +1,15 @@
 ﻿namespace PlannerCRM.Client.Models;
 
-public class CascadingDataContainer
+public class CascadingDataContainer<TItem>
+    where TItem : class, new()
 {
-    public bool IsSelectSelected { get; set; }
+    public bool IsItemSelected { get; set; }
     public bool IsAddSelected { get; set; }
     public bool IsUpdateSelected { get; set; }
     public bool IsDeleteSingleItemSelected { get; set; }
     public bool IsDeleteMultipleSelected { get; set; }
-
     public bool IsOperationDone { get; set; }
+    public TItem SelectedItem { get; set; }
+    public IEnumerable<TItem> SelectedItems { get; set; }
+    public TItem NewItem { get; set; }
 }
