d79dfa15becc291e66e636d555c38bf5bed171ac
liviusilion-dev <silionliviu001@gmail.com>
2025-01-08 23:09:52 +0100

    Create class container for multiple cascading parameters

---
+++ PlannerCRM/Client/Models/CascadingDataContainer.cs
@@ -0,0 +1,12 @@
+﻿namespace PlannerCRM.Client.Models;
+
+public class CascadingDataContainer
+{
+    public bool IsSelectSelected { get; set; }
+    public bool IsAddSelected { get; set; }
+    public bool IsUpdateSelected { get; set; }
+    public bool IsDeleteSingleItemSelected { get; set; }
+    public bool IsDeleteMultipleSelected { get; set; }
+
+    public bool IsOperationDone { get; set; }
+}
