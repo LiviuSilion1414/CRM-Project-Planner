84963630876d0c11addaa7da7ddce53103832307
liviusilion-dev <silionliviu001@gmail.com>
2025-01-09 21:30:24 +0100

    Add Entity helper to initialize data

---
+++ PlannerCRM/Client/Utilities/EntityHelper.cs
@@ -0,0 +1,32 @@
+﻿namespace PlannerCRM.Client.Utilities;
+
+public static class EntityHelper<TItem>
+    where TItem : class, new()
+{
+
+    public static void InitializePropertiesRecursively(object obj)
+    {
+        if (obj == null)
+            return;
+
+        var properties = obj.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        foreach (var prop in properties)
+        {
+            if (!prop.CanWrite)
+                continue;
+
+            if (prop.PropertyType.IsValueType || prop.PropertyType == typeof(string))
+                continue;
+
+            var value = prop.GetValue(obj);
+            if (value == null)
+            {
+                value = Activator.CreateInstance(prop.PropertyType);
+                prop.SetValue(obj, value);
+            }
+
+            InitializePropertiesRecursively(value);
+        }
+    }
+}
