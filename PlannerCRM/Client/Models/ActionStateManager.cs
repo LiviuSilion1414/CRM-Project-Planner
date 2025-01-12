37756d5a4b9cdf0b182e2ba5a191b2c5f6ac4ae9
liviusilion-dev <silionliviu001@gmail.com>
2025-01-12 14:42:14 +0100

    Use SRP principle
    Separate concerns and responsabilities

---
+++ PlannerCRM/Client/Models/ActionStateManager.cs
@@ -0,0 +1,16 @@
+﻿namespace PlannerCRM.Client.Models;
+
+public enum ActionState
+{
+    None,
+    Add,
+    Update,
+    DeleteSingle,
+    DeleteMultiple
+}
+
+public class ActionStateManager
+{
+    public ActionState CurrentState { get; set; } = ActionState.None;
+    public bool IsOperationDone { get; set; } = false;
+}
--- PlannerCRM/Client/Models/CascadingDataContainer.cs
+++ PlannerCRM/Client/Models/CascadingDataContainer.cs
@@ -4,18 +4,7 @@ public class CascadingDataContainer<TItem, TTemp>
     where TItem : class, new()
     where TTemp : class, new()
 {
-    public bool IsItemSelected { get; set; } = false;
-    public bool IsAddSelected { get; set; } = false;
-    public bool IsUpdateSelected { get; set; } = false;
-    public bool IsDeleteSingleItemSelected { get; set; } = false;
-    public bool IsDeleteMultipleSelected { get; set; } = false;
-    public bool IsOperationDone { get; set; } = false;
-    public string Query { get; set; } = string.Empty;
-    public TItem SelectedItem { get; set; } = new();
-    public TItem NewItem { get; set; } = new();
-    public TTemp TempItem { get; set; } = new();
-    public List<string> SelectedProperties { get; set; } = [];
-    public List<TItem> MainItems { get; set; } = [];
-    public List<TTemp> TempItems { get; set; } = [];
-    public List<TItem> SelectedItems { get; set; } = [];
+    public ActionStateManager ActionStateManager { get; set; } = new();
+    public QueryManager QueryManager { get; set; } = new();
+    public DataManager<TItem, TTemp> DataManager { get; set; } = new();
 }
