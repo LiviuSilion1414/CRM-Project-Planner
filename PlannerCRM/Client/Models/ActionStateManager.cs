cdd666d7c172f024b95c8ea271528f77bd8a51bd
liviusilion-dev <silionliviu001@gmail.com>
2025-01-12 14:55:50 +0100

    Update ActionStateManager workflow

--- PlannerCRM/Client/Models/ActionStateManager.cs
+++ PlannerCRM/Client/Models/ActionStateManager.cs
@@ -1,8 +1,9 @@
 ﻿namespace PlannerCRM.Client.Models;
 
-public enum ActionState
+public enum ActionType
 {
     None,
+    View,
     Add,
     Update,
     DeleteSingle,
@@ -11,6 +12,45 @@ public enum ActionState
 
 public class ActionStateManager
 {
-    public ActionState CurrentState { get; set; } = ActionState.None;
-    public bool IsOperationDone { get; set; } = false;
+    public bool IsAddSelected { get; private set; }
+    public bool IsViewSelected { get; private set; }
+    public bool IsUpdateSelected { get; private set; }
+    public bool IsDeleteSingleSelected { get; private set; }
+    public bool IsDeleteMultipleSelected { get; private set; }
+    public bool IsOperationDone { get; set; }
+
+    public void SetAction(ActionType actionType)
+    {
+        ResetFlags();
+
+        switch (actionType)
+        {
+            case ActionType.View:
+                IsViewSelected = true; 
+                break;
+            case ActionType.Add:
+                IsAddSelected = true;
+                break;
+            case ActionType.Update:
+                IsUpdateSelected = true;
+                break;
+            case ActionType.DeleteSingle:
+                IsDeleteSingleSelected = true;
+                break;
+            case ActionType.DeleteMultiple:
+                IsDeleteMultipleSelected = true;
+                break;
+            case ActionType.None:
+            default:
+                break;
+        }
+    }
+
+    private void ResetFlags()
+    {
+        IsAddSelected = false;
+        IsUpdateSelected = false;
+        IsDeleteSingleSelected = false;
+        IsDeleteMultipleSelected = false;
+    }
 }
