45881e1f794cd8dfa0b33565fa146246bfa4a2c2
liviusilion-dev <silionliviu001@gmail.com>
2025-01-15 21:38:51 +0100

    refactor: remove code duplication

--- PlannerCRM/Client/Models/CrudApiManager.cs
+++ PlannerCRM/Client/Models/CrudApiManager.cs
@@ -4,7 +4,7 @@ public class CrudApiManager
 {
     public const string ADD = "add";
     public const string EDIT = "edit";
-    public const string DELETE = "add";
+    public const string DELETE = "delete";
     public const string GET_BY_ID = "getById";
     public const string GET_WITH_PAGINATION = "getWithPagination";
 }
