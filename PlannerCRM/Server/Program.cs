42176c8016c59d6663decb52dd46bef4d9a16da8
liviusilion-dev <silionliviu001@gmail.com>
2025-01-18 14:15:28 +0100

    hot-fix: Use WebAssembly debugging for development environment

--- PlannerCRM/Server/Program.cs
+++ PlannerCRM/Server/Program.cs
@@ -21,9 +21,14 @@ var app = builder.Build();
 
 app.UseMiddleware<CustomExceptionMiddleware>();
 
-if (!app.Environment.IsDevelopment()) {
+if (!app.Environment.IsDevelopment()) 
+{
     app.UseExceptionHandler("/Error");
     app.UseHsts();
+} 
+else
+{
+    app.UseWebAssemblyDebugging();
 }
 
 app.UseHttpsRedirection();
