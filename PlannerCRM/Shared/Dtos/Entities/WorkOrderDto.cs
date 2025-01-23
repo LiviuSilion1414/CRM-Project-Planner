17dc5d23a96a6a94c5d5f693ffb743f257fbe0e3
liviusilion-dev <silionliviu001@gmail.com>
2025-01-24 00:50:36 +0100

    Remove detached prop

--- PlannerCRM/Shared/Dtos/Entities/WorkOrderDto.cs
+++ PlannerCRM/Shared/Dtos/Entities/WorkOrderDto.cs
@@ -26,6 +26,6 @@ public class WorkOrderDto
    
     [Required]
     public FirmClientDto FirmClient { get; set; }
-    public WorkOrderCostDto WorkOrderCost { get; set; }
+    //public WorkOrderCostDto WorkOrderCost { get; set; }
     public List<ActivityDto> Activities { get; set; }
 }
