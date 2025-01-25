7c8b26f2f2b4802ec48f525273af6d51223cf9e6
liviusilion-dev <silionliviu001@gmail.com>
2025-01-25 19:45:34 +0100

    Add custom type converter aimed for collection mapping

--- PlannerCRM/Server/Profiles/FirmClientProfile.cs
+++ PlannerCRM/Server/Profiles/FirmClientProfile.cs
@@ -1,10 +1,16 @@
-﻿namespace PlannerCRM.Server.Profiles;
+﻿using PlannerCRM.Server.Utilities;
+using System.Security.AccessControl;
+
+namespace PlannerCRM.Server.Profiles;
 
 public class FirmClientProfile : Profile
 {
     public FirmClientProfile()
     {
-        CreateMap<FirmClient, FirmClientDto>().MaxDepth(1);
+        CreateMap<FirmClient, FirmClientDto>()
+            .MaxDepth(1)
+            .ForMember(dest => dest.WorkOrders, cfg => cfg.Ignore())
+            .AfterMap((s, d) => CustomTypeConverter<ICollection<WorkOrder>, List<WorkOrderDto>>.Convert(s.WorkOrders));
         CreateMap<FirmClientDto, FirmClient>().MaxDepth(1);
     }
 }
