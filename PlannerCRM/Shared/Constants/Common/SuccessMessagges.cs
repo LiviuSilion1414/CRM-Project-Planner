3f80029445e31feb6ae18560c75e3c7f5e3f7362
liviusilion-dev <silionliviu001@gmail.com>
2025-01-28 11:27:57 +0100

    Use notifications after handling data

---
+++ PlannerCRM/Shared/Constants/Common/SuccessMessagges.cs
@@ -0,0 +1,12 @@
+namespace PlannerCRM.Shared.Constants.Common;
+
+public class SuccessMessagges
+{
+    public const string ITEM_ADD = "Item added successfully!";
+    public const string ITEM_EDIT = "Item updated successfully!";
+    public const string ITEM_DELETE = "Item deleted successfully!";
+    public const string ITEM_ARCHIVE = "Item archived successfully!";
+    public const string ITEM_RESTORE = "Item restored successfully!";
+
+    public const string REPORT_CREATED = "Report created successfully!";
+}
